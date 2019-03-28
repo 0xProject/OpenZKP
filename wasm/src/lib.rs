319d72f3ee9e89d93d522a2073cb8e3dc828f5c1
Remco Bloemen <remco@0x.org>
2019-03-28 15:34:58 -0700

    Add sign and verify bindings

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,8 +2,8 @@ mod utils;
 
 use cfg_if::cfg_if;
 use starkcrypto;
-use starkcrypto::num::BigUint;
 use wasm_bindgen::prelude::*;
+use serde::{Serialize, Deserialize};
 
 cfg_if! {
     // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -14,48 +14,71 @@ cfg_if! {
     }
 }
 
+fn from_string(s: &str) -> [u8;32] {
+    // TODO: Skip '0x' prefix
+    // TODO: Decoding error.
+    let h = hex::decode(s).unwrap();
+    let mut array = [0; 32];
+    array.copy_from_slice(h.as_slice());
+    array
+}
+
+fn to_string(b: &[u8;32]) -> String {
+    hex::encode(b)
+}
+
 #[wasm_bindgen]
 pub fn nop(a: &str, b: &str) -> String {
     let elements = [
-        BigUint::parse_bytes(a.as_bytes(), 16).unwrap(),
-        BigUint::parse_bytes(b.as_bytes(), 16).unwrap(),
+        from_string(a),
+        from_string(b),
     ];
     let h = elements[1].clone();
-    h.to_str_radix(16)
+    to_string(&h)
 }
 
 #[wasm_bindgen]
 pub fn pedersen_hash(a: &str, b: &str) -> String {
-    let elements = [
-        BigUint::parse_bytes(a.as_bytes(), 16).unwrap(),
-        BigUint::parse_bytes(b.as_bytes(), 16).unwrap(),
-    ];
-    let h = starkcrypto::pedersen::hash(&elements);
-    h.to_str_radix(16)
+    to_string(&starkcrypto::hash(&from_string(a), &from_string(b)))
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Signature {
+    r: String,
+    w: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CurvePoint {
+    x: String,
+    y: String,
 }
 
 #[wasm_bindgen]
-pub fn sign(message_hash: &str, private_key: &str) -> Box<[JsValue]> {
-    let m = BigUint::parse_bytes(message_hash.as_bytes(), 16).unwrap();
-    let k = BigUint::parse_bytes(private_key.as_bytes(), 16).unwrap();
-    let (r, w) = starkcrypto::ecdsa::sign(&m, &k);
-    vec![
-        JsValue::from_str(&r.to_str_radix(16)),
-        JsValue::from_str(&w.to_str_radix(16)),
-    ]
-    .into_boxed_slice()
+pub fn public_key(private_key: &str) -> JsValue {
+    let (x, y) = starkcrypto::public_key(&from_string(private_key));
+    JsValue::from_serde(&CurvePoint {
+        x: to_string(&x),
+        y: to_string(&y),
+    }).unwrap()
 }
 
-/*
 #[wasm_bindgen]
-pub fn verify(message_hash: &str, signature: JsValue, public_key: JsValue) -> bool {
-
-    let m = BigUint::parse_bytes(message_hash.as_bytes(), 16).unwrap();
-    let k = BigUint::parse_bytes(private_key.as_bytes(), 16).unwrap();
-    let (r, w) = starkcrypto::ecdsa::sign(&m, &k);
-    vec![
-        JsValue::from_str(&r.to_str_radix(16)),
-        JsValue::from_str(&w.to_str_radix(16)),
-    ].into_boxed_slice()
+pub fn sign(message_hash: &str, private_key: &str) -> JsValue {
+    let (r, w) = starkcrypto::sign(&from_string(message_hash), &from_string(private_key));
+    JsValue::from_serde(&Signature {
+        r: to_string(&r),
+        w: to_string(&w),
+    }).unwrap()
 }
-*/
+
+#[wasm_bindgen]
+pub fn verify(message_hash: &str, signature: &JsValue, public_key: &JsValue) -> bool {
+    let s: Signature = signature.into_serde().unwrap();
+    let p: CurvePoint = public_key.into_serde().unwrap();
+    let r = from_string(&s.r);
+    let w = from_string(&s.w);
+    let x = from_string(&p.x);
+    let y = from_string(&p.y);
+    starkcrypto::verify(&from_string(message_hash), (&r, &w), (&x, &y))
+}
--- wasm/tests/web.rs
+++ wasm/tests/web.rs
@@ -2,10 +2,9 @@
 
 #![cfg(target_arch = "wasm32")]
 
-extern crate wasm_bindgen_test;
-use starkcrypto::num::BigUint;
-use starkcrypto::pedersen::hash;
+use starkcrypto;
 use wasm_bindgen_test::*;
+use starkcrypto_wasm::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
@@ -16,21 +15,9 @@ fn pass() {
 
 #[wasm_bindgen_test]
 fn test_hash_1() {
-    #[rustfmt::skip]
-    let elements = [
-        BigUint::from_slice(&[
-            0x405371cb, 0x28feb561, 0xa1393627, 0x9c53068d,
-            0x1a575610, 0x5caf6453, 0x35c87824, 0x03d937c0,
-        ]),
-        BigUint::from_slice(&[
-            0x9cd8b31a, 0xbbc6aeff, 0x5695e02f, 0x791bf627,
-            0x880906c2, 0xe1e4bbe2, 0x0250e382, 0x0208a0a1,
-        ]),
-    ];
-    #[rustfmt::skip]
-    let result = BigUint::from_slice(&[
-        0x53f3111b, 0x1e311a7c, 0x139a84d0, 0xa8af1bbb,
-        0x57037e4a, 0xfb867eaf, 0x76790645, 0x02d895bd,
-    ]);
-    assert_eq!(hash(&elements), result);
+    let a = "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb";
+    let b = "0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a";
+    let result = pedersen_hash(a, b);
+    let expected = "02d895bd76790645fb867eaf57037e4aa8af1bbb139a84d01e311a7c53f3111b";
+    assert_eq!(expected, result);
 }
