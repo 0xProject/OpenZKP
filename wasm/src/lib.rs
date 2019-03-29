17d1e38632fecd0c786e191a0afa0af24a127dc9
Remco Bloemen <remco@0x.org>
2019-03-29 14:22:55 -0700

    Add wasm bindings

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -51,6 +51,17 @@ pub struct CurvePoint {
     y: String,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct MakerMessage {
+    vault_a: u32,
+    vault_b: u32,
+    amount_a: u64,
+    amount_b: u64,
+    token_a: String,
+    token_b: String,
+    trade_id: u32,
+}
+
 #[wasm_bindgen]
 pub fn public_key(private_key: &str) -> JsValue {
     let (x, y) = starkcrypto::public_key(&from_string(private_key));
@@ -80,4 +91,116 @@ pub fn verify(message_hash: &str, signature: &JsValue, public_key: &JsValue) -> 
     let x = from_string(&p.x);
     let y = from_string(&p.y);
     starkcrypto::verify(&from_string(message_hash), (&r, &w), (&x, &y))
+}
+
+#[wasm_bindgen]
+pub fn maker_hash(message: &JsValue) -> String {
+    let message: MakerMessage = message.into_serde().unwrap();
+    to_string(&starkcrypto::maker_hash(&starkcrypto::MakerMessage {
+        vault_a: message.vault_a,
+        vault_b: message.vault_b,
+        amount_a: message.amount_a,
+        amount_b: message.amount_b,
+        token_a: from_string(&message.token_a),
+        token_b: from_string(&message.token_b),
+        trade_id: message.trade_id,
+    }))
+}
+
+#[wasm_bindgen]
+pub fn taker_hash(maker_message_hash: &str, vault_a: u32, vault_b: u32) -> String {
+    to_string(&starkcrypto::taker_hash(
+        &from_string(maker_message_hash),
+        vault_a,
+        vault_b,
+    ))
+}
+
+#[wasm_bindgen]
+pub fn maker_sign(message: &JsValue, private_key: &str) -> JsValue {
+    let message: MakerMessage = message.into_serde().unwrap();
+    let (r, w) = starkcrypto::maker_sign(
+        &starkcrypto::MakerMessage {
+            vault_a: message.vault_a,
+            vault_b: message.vault_b,
+            amount_a: message.amount_a,
+            amount_b: message.amount_b,
+            token_a: from_string(&message.token_a),
+            token_b: from_string(&message.token_b),
+            trade_id: message.trade_id,
+        },
+        &from_string(private_key),
+    );
+    JsValue::from_serde(&Signature {
+        r: to_string(&r),
+        w: to_string(&w),
+    })
+    .unwrap()
+}
+
+#[wasm_bindgen]
+pub fn taker_sign(
+    maker_message_hash: &str,
+    vault_a: u32,
+    vault_b: u32,
+    private_key: &str,
+) -> JsValue {
+    let (r, w) = starkcrypto::taker_sign(
+        &from_string(&maker_message_hash),
+        vault_a,
+        vault_b,
+        &from_string(private_key),
+    );
+    JsValue::from_serde(&Signature {
+        r: to_string(&r),
+        w: to_string(&w),
+    })
+    .unwrap()
+}
+
+#[wasm_bindgen]
+pub fn maker_verify(message: &JsValue, signature: &JsValue, public_key: &JsValue) -> bool {
+    let message: MakerMessage = message.into_serde().unwrap();
+    let s: Signature = signature.into_serde().unwrap();
+    let p: CurvePoint = public_key.into_serde().unwrap();
+    let r = from_string(&s.r);
+    let w = from_string(&s.w);
+    let x = from_string(&p.x);
+    let y = from_string(&p.y);
+    starkcrypto::maker_verify(
+        &starkcrypto::MakerMessage {
+            vault_a: message.vault_a,
+            vault_b: message.vault_b,
+            amount_a: message.amount_a,
+            amount_b: message.amount_b,
+            token_a: from_string(&message.token_a),
+            token_b: from_string(&message.token_b),
+            trade_id: message.trade_id,
+        },
+        (&r, &w),
+        (&x, &y),
+    )
+}
+
+#[wasm_bindgen]
+pub fn taker_verify(
+    make_message_hash: &str,
+    vault_a: u32,
+    vault_b: u32,
+    signature: &JsValue,
+    public_key: &JsValue,
+) -> bool {
+    let s: Signature = signature.into_serde().unwrap();
+    let p: CurvePoint = public_key.into_serde().unwrap();
+    let r = from_string(&s.r);
+    let w = from_string(&s.w);
+    let x = from_string(&p.x);
+    let y = from_string(&p.y);
+    starkcrypto::taker_verify(
+        &from_string(make_message_hash),
+        vault_a,
+        vault_b,
+        (&r, &w),
+        (&x, &y),
+    )
 }
