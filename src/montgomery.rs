054b71e5ddb318f3e33e559399dcdee5aa8bc6bc
Remco Bloemen <remco@0x.org>
2019-04-04 16:44:37 -0700

    Implement Montgomery multiplication

--- src/montgomery.rs
+++ src/montgomery.rs
@@ -3,6 +3,7 @@ use crate::u256::U256;
 use crate::u256h;
 use crate::utils::{adc, mac};
 use hex_literal::*;
+use std::num::Wrapping;
 
 // M64 = -MODULUS^(-1) mod 2^64
 pub const M64: u64 = 0xffff_ffff_ffff_ffff; // = -1
@@ -57,9 +58,43 @@ pub fn redc(lo: &U256, hi: &U256) -> U256 {
 }
 
 pub fn mul_redc(a: &U256, b: &U256) -> U256 {
-    // TODO: Algorithm 14.36 from Handbook of Applied Cryptography
-    let (lo, hi) = a.mul_full(b);
-    redc(&lo, &hi)
+    // Algorithm 14.36 from Handbook of Applied Cryptography
+    let x = [a.c0, a.c1, a.c2, a.c3];
+    let y = [b.c0, b.c1, b.c2, b.c3];
+    let m = [MODULUS.c0, MODULUS.c1, MODULUS.c2, MODULUS.c3];
+    let mut a = [0u64; 5];
+    for i in 0..4 {
+        let ui = ((Wrapping(a[0]) + Wrapping(x[i]) * Wrapping(y[0])) * Wrapping(M64)).0;
+        // A += x[i] * y
+        let mut carry = 0;
+        for j in 0..4 {
+            let (ai, c) = mac(a[j], x[i], y[j], carry);
+            a[j] = ai;
+            carry = c;
+        }
+        a[4] += carry;
+        // A += ui * m
+        let mut carry = 0;
+        for j in 0..4 {
+            let (ai, c) = mac(a[j], ui, m[j], carry);
+            a[j] = ai;
+            carry = c;
+        }
+        a[4] += carry;
+        // A >>= 64
+        for j in 0..4 {
+            a[j] = a[j + 1];
+        }
+        a[4] = 0;
+        let A = U256::new(a[0], a[1], a[2], a[3]);
+    }
+
+    // Final reduction
+    let mut r = U256::new(a[0], a[1], a[2], a[3]);
+    if r >= MODULUS {
+        r -= &MODULUS;
+    }
+    r
 }
 
 pub fn sqr_redc(a: &U256) -> U256 {
@@ -90,5 +125,13 @@ mod tests {
         let b = u256h!("024385f6bebc1c496e09955db534ef4b1eaff9a78e27d4093cfa8f7c8f886f6b");
         let c = u256h!("012e440f0965e7029c218b64f1010006b5c4ba8b1497c4174a32fec025c197bc");
         assert_eq!(redc(&a, &b), c);
+    }
+
+    #[test]
+    fn test_mul_redc() {
+        let a = u256h!("0548c135e26faa9c977fb2eda057b54b2e0baa9a77a0be7c80278f4f03462d4c");
+        let b = u256h!("024385f6bebc1c496e09955db534ef4b1eaff9a78e27d4093cfa8f7c8f886f6b");
+        let c = u256h!("012b854fc6321976d374ad069cfdec8bb7b2bd184259dae8f530cbb28f0805b4");
+        assert_eq!(mul_redc(&a, &b), c);
     }
 }
