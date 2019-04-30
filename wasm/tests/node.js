1c80233e8605e529be54120fc6f26cd72361953f
Remco Bloemen <remco@0x.org>
2019-04-29 23:54:48 -0700

    Update testcases

--- wasm/tests/node.js
+++ wasm/tests/node.js
@@ -1,17 +1,29 @@
 var starkcrypto = require("../pkg/starkcrypto_wasm");
 
+// Deep comparisson for simple objects.
+function deepEqual(x, y) {
+    const ok = Object.keys,
+        tx = typeof x,
+        ty = typeof y;
+    return x && y && tx === "object" && tx === ty
+        ? ok(x).length === ok(y).length &&
+              ok(x).every(key => deepEqual(x[key], y[key]))
+        : x === y;
+}
+
 console.log("StarkCrypto WebAssembly support.");
 starkcrypto.init();
 
 // Test nop
+// (No python equivalent)
 {
-    var result = starkcrypto.nop(
-        "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb",
-        "0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a"
+    const result = starkcrypto.nop(
+        "115792089237316195423570985008687907853269984665640564039457584007913129639935",
+        "3201502841558479033173442359765767139800663714742290109531927391993829786728"
     );
     if (
         result ==
-        "0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a"
+        "3201502841558479033173442359765767139800663714742290109531927391993829786728"
     ) {
         console.log("Nop test succeed.");
     } else {
@@ -20,15 +32,19 @@ starkcrypto.init();
 }
 
 // Test hash
+// python3 ./signature_cli.py hash --msg \
+//   1740729136829561885683894917751815192814966525555656371386868611731128807883 \
+//   919869093895560023824014392670608914007817594969197822578496829435657368346
 {
-    var result = starkcrypto.pedersen_hash(
-        "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb",
-        "0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a"
+    const result = starkcrypto.pedersen_hash(
+        "1740729136829561885683894917751815192814966525555656371386868611731128807883",
+        "919869093895560023824014392670608914007817594969197822578496829435657368346"
     );
-    if (
-        result ==
-        "004cd9415015d53d3d71f13e865a52a70457c60fa534fe0efffe34d2f6af6744"
-    ) {
+    const expected = {
+        msg_hash:
+            "135779816710909020805564054333139590933910151167913961258433275464447387460"
+    };
+    if (deepEqual(result, expected)) {
         console.log("Pedersen hash test succeed.");
     } else {
         console.error("Pedersen hash test failed!");
@@ -36,16 +52,19 @@ starkcrypto.init();
 }
 
 // Test public key
+// python3 ./signature_cli.py priv_to_pub --priv_key \
+//   1740729136829561885683894917751815192814966525555656371386868611731128807883
 {
-    var result = starkcrypto.public_key(
-        "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb"
+    const result = starkcrypto.public_key(
+        "1740729136829561885683894917751815192814966525555656371386868611731128807883"
     );
-    if (
-        result.x ==
-            "02511bef9567504bdf55d49657f8e20b2b4a5d59b32c8983de33f53d3ecc330e" &&
-        result.y ==
-            "069df6eb1781680d36aa740c1f4ce0ff9f960a005362c136960ed48ed373a250"
-    ) {
+    const expected = {
+        x:
+            "1047933115726230933936444839278495959863389921309454379825626157597302993678",
+        y:
+            "2992976248333999363926335380274708371827571188820497486743080013169132020304"
+    };
+    if (deepEqual(result, expected)) {
         console.log("Public key test succeed.");
     } else {
         console.error("Public key test failed!");
@@ -53,17 +72,19 @@ starkcrypto.init();
 }
 
 // Test sign
+// (No python equivalent)
 {
-    var result = starkcrypto.sign(
-        "01e542e2da71b3f5d7b4e9d329b4d30ac0b5d6f266ebef7364bf61c39aac35d0",
-        "03c1e9550e66958296d11b60f8e8e7a7ad990d07fa65d5f7652c4a6c87d4e3cc"
+    const result = starkcrypto.sign(
+        "857382457629205625681214596003761548740910932106684834463954993440159970768",
+        "1699550429262868952957733065396688802326540225623380427551300052767936406476"
     );
-    if (
-        result.r ==
-            "0010eaece1a727f8c64faf2f236943c2691ba8ca34e1da77880586f5c20fcf63" &&
-        result.w ==
-            "077e670848f61ff0a6d7f4f04a4740f8d50dcf8db8e7a4522dc05ef8c2d3ad89"
-    ) {
+    const expected = {
+        r:
+            "29890947993046261611645984470574583259836861919677343317860415270909103971",
+        w:
+            "3389523773481968765502141586752314378581761033301891571958931787092130508169"
+    };
+    if (deepEqual(result, expected)) {
         console.log("Sign test succeed.");
     } else {
         console.error("Sign test failed!");
@@ -71,101 +92,136 @@ starkcrypto.init();
 }
 
 // Test verify
+// python3 ./signature_cli.py verify \
+// --msg_hash 919869093895560023824014392670608914007817594969197822578496829435657368346\
+// --r 874739451078007766457464989774322083649278607533249481151382481072868806602\
+// --w 917056236706666338779046290895782911901388693926022961665551824457713815676\
+// --pub_key_x 1047933115726230933936444839278495959863389921309454379825626157597302993678 \
+// --pub_key_y 2992976248333999363926335380274708371827571188820497486743080013169132020304
 {
-    var correct = starkcrypto.verify(
-        "0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a",
+    const result = starkcrypto.verify(
+        "919869093895560023824014392670608914007817594969197822578496829435657368346",
         {
             r:
-                "01ef15c18599971b7beced415a40f0c7deacfd9b0d1819e03d723d8bc943cfca",
+                "874739451078007766457464989774322083649278607533249481151382481072868806602",
             w:
-                "020709125651d6d1147c4f45e72ecd4848432fa86b3b867c9e7f61b47bcb907c"
+                "917056236706666338779046290895782911901388693926022961665551824457713815676"
         },
         {
             x:
-                "02511bef9567504bdf55d49657f8e20b2b4a5d59b32c8983de33f53d3ecc330e",
+                "1047933115726230933936444839278495959863389921309454379825626157597302993678",
             y:
-                "069df6eb1781680d36aa740c1f4ce0ff9f960a005362c136960ed48ed373a250"
+                "2992976248333999363926335380274708371827571188820497486743080013169132020304"
         }
     );
-    var incorrect = starkcrypto.verify(
-        "0218a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a",
-        {
-            r:
-                "01ef15c18599971b7beced415a40f0c7deacfd9b0d1819e03d723d8bc943cfca",
-            w:
-                "020709125651d6d1147c4f45e72ecd4848432fa86b3b867c9e7f61b47bcb907c"
-        },
-        {
-            x:
-                "02511bef9567504bdf55d49657f8e20b2b4a5d59b32c8983de33f53d3ecc330e",
-            y:
-                "069df6eb1781680d36aa740c1f4ce0ff9f960a005362c136960ed48ed373a250"
-        }
-    );
-    if (correct == true && incorrect == false) {
+    const expected = { is_valid: true };
+    if (deepEqual(result, expected)) {
         console.log("Verify test succeed.");
     } else {
         console.error("Verify test failed!");
     }
 }
 
-// Test maker hash
+// Test maker sign
+// python3 ./signature_cli.py sign_maker --vault_a 21 --vault_b 27
+// --amount_a 6873058723796400 --amount_b 852209057714036
+// --token_a 168976971209324910088270776698114429107164817795147365551345596466024812576\
+// --token_b 210761264384301076547763280170970365712477797880932555831340857495337358698\
+// --trade_id 0 --priv_key 1699550429262868952957733065396688802326540225623380427551300052767936406476
+//
+// NOTE: this requires replacing signature.py line 79 with
+//     k = 0x689bc54bedc93f9acbe9315b4538489c8654a9160d46319f5157b226b8f41ad
 {
-    var result = starkcrypto.maker_hash({
-        vault_a: 21,
-        vault_b: 27,
-        amount_a: 6873058723796400,
-        amount_b: 852209057714036,
-        token_a:
-            "005fa3383597691ea9d827a79e1a4f0f7989c35ced18ca9619de8ab97e661020",
-        token_b:
-            "00774961c824a3b0fb3d2965f01471c9c7734bf8dbde659e0c08dca2ef18d56a",
-        trade_id: 0
-    });
-    if (
-        result ==
-        "035d22e6b67d9dbe893149ede8ae5efb82d1a3f97734689f5189031cc45eebbd"
-    ) {
-        console.log("Maker hash test succeed.");
+    const result = starkcrypto.maker_sign(
+        {
+            vault_a: 21,
+            vault_b: 27,
+            amount_a: "6873058723796400",
+            amount_b: "852209057714036",
+            token_a:
+                "168976971209324910088270776698114429107164817795147365551345596466024812576",
+            token_b:
+                "210761264384301076547763280170970365712477797880932555831340857495337358698",
+            trade_id: 0
+        },
+        "1699550429262868952957733065396688802326540225623380427551300052767936406476"
+    );
+    const expected = {
+        maker_msg:
+            "710471947606888870830612264939157805860689127401500845152678489563118455148",
+        r:
+            "2082956756539342940653267974841073721753715489508559753023107475569635554744",
+        w:
+            "1720329235658045098345293225536236153800035436641504589044650073399453764796"
+    };
+    if (deepEqual(result, expected)) {
+        console.log("Maker sign test succeed.");
     } else {
-        console.error("Maker hash test failed!");
+        console.error("Maker sign test failed!");
     }
 }
 
-// Test taker hash
+// Test taker sign
+// python3 ./signature_cli.py sign_maker --vault_a 21 --vault_b 27
+// --amount_a 6873058723796400 --amount_b 852209057714036
+// --token_a 168976971209324910088270776698114429107164817795147365551345596466024812576\
+// --token_b 210761264384301076547763280170970365712477797880932555831340857495337358698\
+// --trade_id 0 --taker_vault_a 2 --taker_vault_b 31 \
+// --priv_key 1043001682421203376945321761381453872933181159161894427363884596240717713089
+//
+// NOTE: this requires replacing signature.py line 79 with
+//     k = 0x1ed2c3d93af13b064784b734f05bfb9838a19eea53ee0b502bf2b20b1661a05
 {
-    var result = starkcrypto.taker_hash(
-        "01c280f77aa5859027c67411b6859584143d49970528bcbd8db131d39ecf7eb1",
+    const result = starkcrypto.taker_sign(
+        {
+            vault_a: 21,
+            vault_b: 27,
+            amount_a: "6873058723796400",
+            amount_b: "852209057714036",
+            token_a:
+                "168976971209324910088270776698114429107164817795147365551345596466024812576",
+            token_b:
+                "210761264384301076547763280170970365712477797880932555831340857495337358698",
+            trade_id: 0
+        },
         2,
-        31
+        31,
+        "1043001682421203376945321761381453872933181159161894427363884596240717713089"
     );
-    if (
-        result ==
-        "024e516a8e5f3a523f7725108516bbded20cb290c21925c95836fd66af4c0ec1"
-    ) {
-        console.log("Taker hash test succeed.");
+    const expected = {
+        maker_msg:
+            "710471947606888870830612264939157805860689127401500845152678489563118455148",
+        taker_msg:
+            "3352318676647792916544761969851899543842156867533342014273537377500814333988",
+        r:
+            "1946337478642587999776598742031228742273738226158101006612665042806856730080",
+        w:
+            "2833477950891576348646763500424705069561372684358745311108148957048765586550"
+    };
+    if (deepEqual(result, expected)) {
+        console.log("Taker sign test succeed.");
     } else {
-        console.error("Taker hash test failed!");
+        console.error("Taker sign test failed!");
     }
 }
 
 // Test order lifecycle test
 {
     var maker_private =
-        "03c1e9550e66958296d11b60f8e8e7a7ad990d07fa65d5f7652c4a6c87d4e3cc";
+        "1699550429262868952957733065396688802326540225623380427551300052767936406476";
     var taker_private =
-        "03c1e9550e66958296d11b60f8e8e7a7ad990d07fa65d5f7652c4a6c87d4e3cc";
+        "2082956756539342940653267974841073721753715489508559753023107475569635554744";
     var maker_public = starkcrypto.public_key(maker_private);
     var taker_public = starkcrypto.public_key(taker_private);
     let order = {
         vault_a: 21,
         vault_b: 27,
-        amount_a: 2154686749748910716,
-        amount_b: 1470242115489520459,
+        amount_a: "6873058723796400",
+        amount_b: "852209057714036",
         token_a:
-            "005fa3383597691ea9d827a79e1a4f0f7989c35ced18ca9619de8ab97e661020",
+            "168976971209324910088270776698114429107164817795147365551345596466024812576",
         token_b:
-            "00774961c824a3b0fb3d2965f01471c9c7734bf8dbde659e0c08dca2ef18d56a",
+            "210761264384301076547763280170970365712477797880932555831340857495337358698",
         trade_id: 0
     };
     var maker_sig = starkcrypto.maker_sign(order, maker_private);
@@ -189,8 +245,8 @@ starkcrypto.init();
 console.time("Benchmark 100k nop");
 for (var i = 0; i < 100000; i++) {
     var result = starkcrypto.nop(
-        "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb",
-        "0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a"
+        "115792089237316195423570985008687907853269984665640564039457584007913129639935",
+        "3201502841558479033173442359765767139800663714742290109531927391993829786728"
     );
 }
 console.timeEnd("Benchmark 100k nop");
@@ -199,8 +255,8 @@ console.timeEnd("Benchmark 100k nop");
 console.time("Benchmark 10k hash");
 for (var i = 0; i < 10000; i++) {
     var result = starkcrypto.pedersen_hash(
-        "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb",
-        "0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a"
+        "1740729136829561885683894917751815192814966525555656371386868611731128807883",
+        "919869093895560023824014392670608914007817594969197822578496829435657368346"
     );
 }
 console.timeEnd("Benchmark 10k hash");
@@ -209,7 +265,7 @@ console.timeEnd("Benchmark 10k hash");
 console.time("Benchmark 10k public key");
 for (var i = 0; i < 10000; i++) {
     var result = starkcrypto.public_key(
-        "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb"
+        "1740729136829561885683894917751815192814966525555656371386868611731128807883"
     );
 }
 console.timeEnd("Benchmark 10k public key");
@@ -218,8 +274,8 @@ console.timeEnd("Benchmark 10k public key");
 console.time("Benchmark 10k sign");
 for (var i = 0; i < 10000; i++) {
     var result = starkcrypto.sign(
-        "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb",
-        "03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb"
+        "857382457629205625681214596003761548740910932106684834463954993440159970768",
+        "1699550429262868952957733065396688802326540225623380427551300052767936406476"
     );
 }
 console.timeEnd("Benchmark 10k sign");
@@ -228,18 +284,18 @@ console.timeEnd("Benchmark 10k sign");
 console.time("Benchmark 10k verify");
 for (var i = 0; i < 1000; i++) {
     var result = starkcrypto.verify(
-        "0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a",
+        "919869093895560023824014392670608914007817594969197822578496829435657368346",
         {
             r:
-                "01ef15c18599971b7beced415a40f0c7deacfd9b0d1819e03d723d8bc943cfca",
+                "874739451078007766457464989774322083649278607533249481151382481072868806602",
             w:
-                "020709125651d6d1147c4f45e72ecd4848432fa86b3b867c9e7f61b47bcb907c"
+                "917056236706666338779046290895782911901388693926022961665551824457713815676"
         },
         {
             x:
-                "02511bef9567504bdf55d49657f8e20b2b4a5d59b32c8983de33f53d3ecc330e",
+                "1047933115726230933936444839278495959863389921309454379825626157597302993678",
             y:
-                "069df6eb1781680d36aa740c1f4ce0ff9f960a005362c136960ed48ed373a250"
+                "2992976248333999363926335380274708371827571188820497486743080013169132020304"
         }
     );
 }
@@ -251,12 +307,12 @@ for (var i = 0; i < 1000; i++) {
     var result = starkcrypto.maker_hash({
         vault_a: 21,
         vault_b: 27,
-        amount_a: 2154686749748910716,
-        amount_b: 1470242115489520459,
+        amount_a: "6873058723796400",
+        amount_b: "852209057714036",
         token_a:
-            "005fa3383597691ea9d827a79e1a4f0f7989c35ced18ca9619de8ab97e661020",
+            "168976971209324910088270776698114429107164817795147365551345596466024812576",
         token_b:
-            "00774961c824a3b0fb3d2965f01471c9c7734bf8dbde659e0c08dca2ef18d56a",
+            "210761264384301076547763280170970365712477797880932555831340857495337358698",
         trade_id: 0
     });
 }
@@ -266,18 +322,18 @@ console.timeEnd("Benchmark 1k maker_hash");
 console.time("Benchmark 1k maker taker sign");
 for (var i = 0; i < 1000; i++) {
     var maker_private =
-        "03c1e9550e66958296d11b60f8e8e7a7ad990d07fa65d5f7652c4a6c87d4e3cc";
+        "168976971209324910088270776698114429107164817795147365551345596466024812576";
     var taker_private =
-        "03c1e9550e66958296d11b60f8e8e7a7ad990d07fa65d5f7652c4a6c87d4e3cc";
+        "210761264384301076547763280170970365712477797880932555831340857495337358698";
     let order = {
         vault_a: 21,
         vault_b: 27,
-        amount_a: 2154686749748910716,
-        amount_b: 1470242115489520459,
+        amount_a: "6873058723796400",
+        amount_b: "852209057714036",
         token_a:
-            "005fa3383597691ea9d827a79e1a4f0f7989c35ced18ca9619de8ab97e661020",
+            "168976971209324910088270776698114429107164817795147365551345596466024812576",
         token_b:
-            "00774961c824a3b0fb3d2965f01471c9c7734bf8dbde659e0c08dca2ef18d56a",
+            "210761264384301076547763280170970365712477797880932555831340857495337358698",
         trade_id: 0
     };
     var maker_sig = starkcrypto.maker_sign(order, maker_private);
