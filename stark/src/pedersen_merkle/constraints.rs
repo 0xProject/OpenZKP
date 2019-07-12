9af2431d97c030c9ef873ae5636dcfd251e712b0
Mason Liang <mason@0x.org>
2019-07-12 09:57:54 -0400

    wip

--- stark/src/pedersen_merkle/constraints.rs
+++ stark/src/pedersen_merkle/constraints.rs
@@ -4,6 +4,7 @@ use primefield::{invert_batch, FieldElement};
 use u256::U256;
 // use u256::u256h;
 use ecc::Affine;
+use itertools::izip;
 use starkdex::SHIFT_POINT;
 
 struct Row {
@@ -25,7 +26,7 @@ pub fn eval_c_direct(
     polynomials: &[&[FieldElement]],
     _claim_index: usize,
     _claim: FieldElement,
-    _coefficients: &[FieldElement],
+    coefficients: &[FieldElement],
 ) -> FieldElement {
     let public_input = get_public_input();
     let path_length = U256::from(public_input.path_length as u64);
@@ -36,17 +37,17 @@ pub fn eval_c_direct(
     let trace_generator = FieldElement::root(trace_length.clone()).unwrap();
     let evaluation_generator = FieldElement::root(evaluation_length.clone()).unwrap();
 
-    let _numerators = vec![
-        x - trace_generator.pow(&trace_length - U256::from(1u64)),
+    let numerators = vec![
+        x - trace_generator.pow(&trace_length - U256::ONE),
         x.pow(path_length.clone())
-            - trace_generator.pow((&trace_length - U256::from(1u64)) * &path_length),
+            - trace_generator.pow((&trace_length - U256::ONE) * &path_length),
         FieldElement::ONE,
     ];
-    let _denominators = invert_batch(&[
+    let denominators = invert_batch(&[
         x - FieldElement::ONE,
         x - trace_generator.pow(&trace_length - U256::from(1u64)),
         x.pow(path_length.clone())
-            - trace_generator.pow(&path_length * (&trace_length - U256::from(1u64))),
+            - trace_generator.pow(&path_length * (&trace_length - U256::ONE)),
         x.pow(path_length.clone()) - FieldElement::ONE,
         x.pow(trace_length.clone()) - FieldElement::ONE,
         x.pow(path_length.clone()) - trace_generator.pow(U256::from(252u64) * &path_length),
@@ -92,7 +93,7 @@ pub fn eval_c_direct(
         },
     };
     let left_bit = &this.left.source - next.left.source.double();
-    let _right_bit = &this.right.source - next.right.source.double();
+    let right_bit = &this.right.source - next.right.source.double();
 
     let (shift_point_x, shift_point_y) = match SHIFT_POINT {
         Affine::Zero => panic!(),
@@ -104,7 +105,7 @@ pub fn eval_c_direct(
     let q_x_right = FieldElement::ONE;
     let q_y_right = FieldElement::ZERO;
 
-    let _constraints = vec![
+    let constraints = vec![
         this.left.source.clone(),
         this.left.slope.clone(),
         this.left.x.clone(),
@@ -138,59 +139,64 @@ pub fn eval_c_direct(
         this.right.source.clone(),
     ];
 
-    FieldElement::ZERO
-    // let eval_C0 = |x: FieldElement| -> FieldElement {
-    //     ((eval_P0(&x * &g) - eval_P1(x.clone())) * (&x -
-    // &g.pow(U256::from(trace_len - 1))))         / (&x.pow(U256::
-    // from(trace_len)) - FieldElement::ONE) };
-    // let eval_C1 = |x: FieldElement| -> FieldElement {
-    //     ((eval_P1(&x * &g) - eval_P0(x.clone()) - eval_P1(x.clone()))
-    //         * (&x - (&g.pow(U256::from(trace_len - 1)))))
-    //         / (&x.pow(U256::from(trace_len)) - FieldElement::ONE)
-    // };
-    // let eval_C2 = |x: FieldElement| -> FieldElement {
-    //     ((eval_P0(x.clone()) - FieldElement::ONE) * FieldElement::ONE) / (&x -
-    // FieldElement::ONE) };
-    // let eval_C3 = |x: FieldElement| -> FieldElement {
-    //     (eval_P0(x.clone()) - claim) / (&x - &g.pow(U256::from(claim_index as
-    // u64))) };
-    //
-    // fn degree_adjustment_factor(
-    //     constraint_degree: u64,
-    //     numerator_degree: u64,
-    //     denominator_degree: u64,
-    // ) -> u64 {
-    //     trace_len + denominator_degree - 1 - constraint_degree - numerator_degree
-    // };
-    //
-    // let eval_C = |x: FieldElement| -> FieldElement {
-    //     let composition_degree_bound = trace_len;
-    //     let mut r = FieldElement::ZERO;
-    //     r += &constraint_coefficients[0] * &eval_C0(x.clone());
-    //     r += &constraint_coefficients[1]
-    //         * &eval_C0(x.clone())
-    //         * (&x).pow(U256::from(degree_adjustment_factor(
-    //           composition_degree_bound, trace_len - 1, 1, trace_len,
-    //         )));
-    //     r += &constraint_coefficients[2] * &eval_C1(x.clone());
-    //     r += &constraint_coefficients[3]
-    //         * &eval_C1(x.clone())
-    //         * (&x).pow(U256::from(degree_adjustment_factor(
-    //           composition_degree_bound, trace_len - 1, 1, trace_len,
-    //         )));
-    //     r += &constraint_coefficients[4] * &eval_C2(x.clone());
-    //     r += &constraint_coefficients[5]
-    //         * &eval_C2(x.clone())
-    //         * x.pow(U256::from(degree_adjustment_factor(
-    //           composition_degree_bound, trace_len - 1, 0, 1,
-    //         )));
-    //     r += &constraint_coefficients[6] * (eval_C3.clone())(x.clone());
-    //     r += &constraint_coefficients[7]
-    //         * &eval_C3(x.clone())
-    //         * x.pow(U256::from(degree_adjustment_factor(
-    //           composition_degree_bound, trace_len - 1, 0, 1,
-    //         )));
-    //     r
-    // };
-    // eval_C(x.clone())
+    let degree_adjustment = |constraint_degree: U256,
+                             numerator_degree: U256,
+                             denominator_degree: U256|
+     -> U256 {
+        trace_length.clone() + denominator_degree - U256::ONE - constraint_degree - numerator_degree
+    };
+
+    let adjustments = vec![
+        x.pow(degree_adjustment(
+            &trace_length - U256::ONE,
+            U256::ZERO,
+            U256::ZERO,
+        )),
+        x.pow(degree_adjustment(
+            2u64 * (&trace_length - U256::ONE),
+            U256::ZERO,
+            U256::ONE,
+        )),
+        x.pow(degree_adjustment(
+            &trace_length - U256::ONE,
+            U256::ZERO,
+            U256::ONE,
+        )),
+        x.pow(degree_adjustment(
+            2u64 * (&trace_length - U256::ONE),
+            U256::ONE,
+            path_length.clone(),
+        )),
+        x.pow(degree_adjustment(
+            &trace_length - U256::ONE,
+            U256::ZERO,
+            path_length.clone(),
+        )),
+        x.pow(degree_adjustment(
+            2u64 * (&trace_length - U256::ONE),
+            path_length.clone(),
+            trace_length.clone(),
+        )),
+    ];
+
+    let numerator_indices = vec![
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2, 2,
+    ];
+    let denominator_indices = vec![
+        6, 6, 6, 6, 6, 6, 6, 6, 0, 1, 2, 3, 3, 4, 4, 4, 4, 4, 4, 5, 2, 4, 4, 4, 4, 4, 4, 5, 2,
+    ];
+    let adjustment_indices = vec![
+        0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 4, 5, 5, 5, 5, 5, 5, 4, 4, 5, 5, 5, 5, 5, 5, 4, 4,
+    ];
+
+    let mut result = FieldElement::ZERO;
+    for (i, (numerator_index, denominator_index, adjustment_index)) in
+        izip!(numerator_indices, denominator_indices, adjustment_indices).enumerate()
+    {
+        let value =
+            &constraints[i] * &numerators[numerator_index] * &denominators[denominator_index];
+        result += value
+            * (&coefficients[2 * i] + &coefficients[2 * i + 1] * &adjustments[adjustment_index]);
+    }
+    result
 }
