79fb552deaa24dc3c40eda453e63756b76661292
Remco Bloemen <remco@0x.org>
2019-08-05 15:25:57 -0700

    Proof system

--- stark/src/rational_expression.rs
+++ stark/src/rational_expression.rs
@@ -1,7 +1,7 @@
 use primefield::FieldElement;
-use std::ops::{Add, Div, Mul, Sub};
+use std::ops::{Add, Div, Index, Mul, Sub};
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub enum RationalExpression {
     X,
     Constant(FieldElement),
@@ -13,6 +13,12 @@ pub enum RationalExpression {
     Exp(Box<RationalExpression>, usize),
 }
 
+impl RationalExpression {
+    pub fn pow(&self, exponent: usize) -> RationalExpression {
+        RationalExpression::Exp(Box::new(self.clone()), exponent)
+    }
+}
+
 impl From<i32> for RationalExpression {
     fn from(value: i32) -> Self {
         RationalExpression::Constant(value.into())
@@ -20,7 +26,7 @@ impl From<i32> for RationalExpression {
 }
 
 impl From<&FieldElement> for RationalExpression {
-    fn from(value: FieldElement) -> Self {
+    fn from(value: &FieldElement) -> Self {
         RationalExpression::Constant(value.clone())
     }
 }
@@ -74,11 +80,7 @@ impl RationalExpression {
         match self {
             X => (1, 0),
             Constant(_) => (0, 0),
-            Trace(_i, exp) => {
-                let (n, d) = exp.degree(trace_degree);
-                assert!(d == 0); // TODO: Is there a meaningful use?
-                (n * trace_degree, 0)
-            }
+            Trace(..) => (trace_degree, 0),
             Add(a, b) => {
                 let (an, ad) = a.degree(trace_degree);
                 let (bn, bd) = b.degree(trace_degree);
@@ -96,29 +98,24 @@ impl RationalExpression {
                 let (n, d) = a.degree(trace_degree);
                 (d, n)
             }
-            _ => unimplemented!(),
         }
-    }
-
-    /// Returns the number of columns.
-    pub fn columns(&self) -> usize {
-        unimplemented!() // TODO
     }
 
     pub fn eval(
         &self,
         trace_table: &Fn(usize, &FieldElement) -> FieldElement,
         x: &FieldElement,
+        g: &FieldElement,
     ) -> FieldElement {
         use RationalExpression::*;
         match self {
             X => x.clone(),
             Constant(value) => value.clone(),
-            Trace(i, exp) => trace_table(*i, &exp.eval(trace_table, x)),
-            Add(a, b) => a.eval(trace_table, x) + b.eval(trace_table, x),
-            Neg(a) => -&a.eval(trace_table, x),
+            Trace(&i, &j) => trace_table(i, x * g.pow(j)),
+            Add(a, b) => a.eval(trace_table, x, g) + b.eval(trace_table, x, g),
+            Neg(a) => -&a.eval(trace_table, x, g),
             Inv(a) => {
-                a.eval(trace_table, x)
+                a.eval(trace_table, x, g)
                     .inv()
                     .expect("Division by zero while evaluating RationalExpression.")
             }
@@ -127,48 +124,109 @@ impl RationalExpression {
     }
 }
 
+// Constraints
+
 struct Constraints {
     trace_degree: usize,
+    num_columns:  usize,
     constraints:  Vec<RationalExpression>,
 }
 
+impl Constraints {
+    pub fn combine(&mut self, coefficients: &[FieldElement]) {
+        assert_eq!(coefficients.len(), 2 * self.constraints.len());
+    }
+}
+
+// Trace table
+
+struct TraceTable {
+    trace_length: usize,
+    num_columns:  usize,
+    values:       Vec<FieldElement>,
+}
+
+impl TraceTable {
+    pub fn new(trace_length: usize, num_columns: usize) -> TraceTable {
+        TraceTable {
+            trace_length,
+            num_columns,
+            values: vec![FieldElement::ZERO; trace_length * num_columns],
+        }
+    }
+}
+
+impl Index<usize> for TraceTable {
+    type Output = [FieldElement];
+
+    fn index(&self, i: usize) -> &[FieldElement] {
+        self.values[i * self.num_columns..(i + 1) * self.num_columns]
+    }
+}
+
+impl Index<(usize, usize)> for TraceTable {
+    type Output = FieldElement;
+
+    fn index(&self, (i, j): (usize, usize)) -> &Self::Output {
+        self.values[i * self.num_columns + j]
+    }
+}
+
+// Constraint System
+
+pub trait ConstraintSystem {
+    type PublicInput;
+    type PrivateInput;
+
+    fn constraints(public_input: &Self::PublicInput) -> Constraints;
+
+    fn trace(public_inputs: &Self::PublicInput, private_input: &Self::PrivateInput) -> TraceTable;
+}
 /// The fibonacci constraint system.
 ///
 /// The public inputs are an index and value in the fibonacci sequence.
-#[allow(dead_code)] // TODO
-pub fn fibonacci(index: usize, value: &FieldElement) -> Constraints {
-    use RationalExpression::*;
+struct Fibonacci;
 
-    // Trace table generation
-    let trace_degree = 1024usize;
-    let g = Constant(
-        FieldElement::root(trace_degree.into()).expect("No root of unity for trace degree."),
-    );
-    assert!(index < trace_degree);
+impl ConstraintSystem for Fibonacci {
+    type PrivateInput = FieldElement;
+    type PublicInput = (usize, FieldElement);
 
-    // Constraint repetitions
-    let first_row = RationalExpression::from(1) / (X - 1.into());
-    let target_row = RationalExpression::from(1) / (X - Exp(Box::new(g), index));
-    let every_row = (X - Exp(Box::new(g), trace_degree)) / (X - Exp(Box::new(g), index));
+    fn constraints((&index, value): &Self::PublicInput) -> Constraints {
+        use RationalExpression::*;
 
-    // The system
-    Constraints {
-        trace_degree,
-        constraints: vec![
-            (Trace(0, 0) - 1.into()) * first_row,
-            (Trace(1, 0) - value.into()) * target_row,
-            (Trace(0, 1) - Trace(1, 0)) * every_row,
-            (Trace(1, 1) - Trace(0, 0) - Trace(1, 0)) * every_row,
-        ],
+        // Trace table generation
+        let trace_degree = 1024usize;
+        let num_columns = 2usize;
+        let g = Constant(
+            FieldElement::root(trace_degree.into()).expect("No root of unity for trace degree."),
+        );
+        assert!(index < trace_degree);
+
+        // Constraint repetitions
+        let first_row = RationalExpression::from(1) / (X - 1.into());
+        let target_row = RationalExpression::from(1) / (X - g.pow(index));
+        let every_row = (X - g.pow(trace_degree)) / (X.pow(trace_degree) - 1.into());
+
+        // The system
+        Constraints {
+            trace_degree,
+            num_columns,
+            constraints: vec![
+                (Trace(0, 0) - 1.into()) * first_row,
+                (Trace(1, 0) - value.into()) * target_row,
+                (Trace(0, 1) - Trace(1, 0)) * every_row,
+                (Trace(1, 1) - Trace(0, 0) - Trace(1, 0)) * every_row,
+            ],
+        }
+    }
+
+    fn trace(public_inputs: &Self::PublicInput, private_input: &FieldElement) -> TraceTable {
+        unimplemented!()
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
-    #[test]
-    pub fn eval_fib() {
-        let constraints = fibonacci(1000, &FieldElement::ONE);
-    }
 }
