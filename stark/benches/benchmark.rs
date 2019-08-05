a714e8c505716d5288400f9e2fdc269edce0c0b0
Remco Bloemen <remco@0x.org>
2019-08-05 13:32:13 -0700

    Explicit types

--- stark/benches/benchmark.rs
+++ stark/benches/benchmark.rs
@@ -64,7 +64,7 @@ where
 
 fn merkle_tree_size(crit: &mut Criterion) {
     log_size_bench(crit, "Merkle tree size", &SIZES, move |bench, size| {
-        let leaves: Vec<_> = (0..size).map(|i| U256::from(i)).collect();
+        let leaves: Vec<U256> = (0..size).map(|i| i.into()).collect();
         bench.iter(|| black_box(make_tree(black_box(&leaves))))
     });
 }
@@ -72,7 +72,7 @@ fn merkle_tree_size(crit: &mut Criterion) {
 fn merkle_tree_threads(crit: &mut Criterion) {
     let size: usize = *SIZES.last().unwrap();
     log_thread_bench(crit, "Merkle tree threads", size, move |bench| {
-        let leaves: Vec<_> = (0..size).map(|i| U256::from(i)).collect();
+        let leaves: Vec<U256> = (0..size).map(|i| i.into()).collect();
         bench.iter(|| black_box(make_tree(black_box(&leaves))))
     });
 }
