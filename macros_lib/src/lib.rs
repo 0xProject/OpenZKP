3b14d4a96ba17a893bd0688faa74de671d8b9b0f
Remco Bloemen <remco@0x.org>
2019-08-07 15:18:41 -0700

    Generate limb based representation

--- macros_lib/src/lib.rs
+++ macros_lib/src/lib.rs
@@ -2,6 +2,17 @@ use proc_macro2::{Literal, Span, TokenStream};
 use quote::quote;
 use syn::{Expr, Lit};
 
+// For use in functions that return TokenStream. Turns syn::Result into
+// compile errors.
+macro_rules! handle_errors {
+    ($e:expr) => {
+        match $e {
+            Err(err) => return TokenStream::from(err.to_compile_error()),
+            Ok(val) => val,
+        }
+    };
+}
+
 fn parse_string(input: TokenStream) -> syn::Result<String> {
     let input: Expr = syn::parse2(input)?;
     let result = match input {
@@ -34,28 +45,45 @@ fn parse_hex(input: TokenStream) -> syn::Result<Vec<u8>> {
     })
 }
 
-pub fn hex(input: TokenStream) -> TokenStream {
-    match parse_hex(input) {
-        Ok(bytes) => {
-            let literal = Literal::byte_string(&bytes);
-            quote! { #literal }
-        }
-        Err(err) => TokenStream::from(err.to_compile_error()),
+fn bytes_to_limbs(bytes: &[u8]) -> syn::Result<[u64; 4]> {
+    if bytes.len() > 32 {
+        return Err(syn::Error::new(
+            Span::call_site(),
+            format!("expected up to 32 bytes"),
+        ));
     }
+    let mut result = [0u64; 4];
+    let mut iter = bytes.iter().rev();
+    for i in 0..4 {
+        for j in 0..8 {
+            if let Some(byte) = iter.next() {
+                result[i] |= (*byte as u64) << (8 * j);
+            } else {
+                return Ok(result);
+            }
+        }
+    }
+    Ok(result)
+}
+
+pub fn hex(input: TokenStream) -> TokenStream {
+    let bytes = handle_errors!(parse_hex(input));
+    let literal = Literal::byte_string(&bytes);
+    quote! { #literal }
 }
 
 pub fn u256h(input: TokenStream) -> TokenStream {
-    match parse_hex(input) {
-        Ok(bytes) => {
-            let literal = Literal::byte_string(&bytes);
-            // OPT: Convert to limb based representation.
-            // TODO: Ideally we'd locally import U256 here and
-            // use $crate::U256 here, but this leads to a circular
-            // dependency.
-            quote! { ::u256::U256::from_bytes_be(#literal) }
-        }
-        Err(err) => TokenStream::from(err.to_compile_error()),
-    }
+    let bytes = handle_errors!(parse_hex(input));
+    let limbs = handle_errors!(bytes_to_limbs(bytes.as_slice()));
+    let c0 = Literal::u64_suffixed(limbs[0]);
+    let c1 = Literal::u64_suffixed(limbs[1]);
+    let c2 = Literal::u64_suffixed(limbs[2]);
+    let c3 = Literal::u64_suffixed(limbs[3]);
+
+    // TODO: Ideally we'd locally import U256 here and
+    // use $crate::U256 here, but this leads to a circular
+    // dependency.
+    quote! { ::u256::U256::from_limbs(#c0, #c1, #c2, #c3) }
 }
 
 #[cfg(test)]
@@ -103,11 +131,12 @@ mod test {
     pub fn test_u256h() {
         assert_eq!(
             u256h(quote! {""}).to_string(),
-            quote! {::u256::U256::from_bytes_be(b"")}.to_string()
+            quote! {::u256::U256::from_limbs(0u64, 0u64, 0u64, 0u64)}.to_string()
         );
         assert_eq!(
-            u256h(quote! {"00"}).to_string(),
-            quote! {::u256::U256::from_bytes_be(b"\0")}.to_string()
+            u256h(quote! {"0000000000000004000000000000000300000000000000020000000000000001"})
+                .to_string(),
+            quote! {::u256::U256::from_limbs(1u64, 2u64, 3u64, 4u64)}.to_string()
         );
     }
 }
