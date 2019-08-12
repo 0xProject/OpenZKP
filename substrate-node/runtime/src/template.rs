a85a7302db59e7b7f7bb5173d2d43fbe34a28f5a
Paul Vienhage <paul@0x.org>
2019-08-12 13:51:30 -0400

    fmt

--- substrate-node/runtime/src/template.rs
+++ substrate-node/runtime/src/template.rs
@@ -1,3 +1,5 @@
+#[allow(unused_imports)] // TODO - Remove when used
+use starkdex::wrappers::*;
 /// A runtime module template with necessary imports
 
 /// Feel free to remove or edit this file as needed.
@@ -7,8 +9,6 @@
 /// For more guidance on Substrate modules, see the example module
 /// https://github.com/paritytech/substrate/blob/master/srml/example/src/lib.rs
 use support::{decl_event, decl_module, decl_storage, dispatch::Result, StorageValue};
-#[allow(unused_imports)] // TODO - Remove when used
-use starkdex::wrappers::*;
 use system::ensure_signed;
 
 /// The module's configuration trait.
