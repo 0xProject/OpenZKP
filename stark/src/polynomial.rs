d1c957f0c7e2bebd2378e64bd52cbf34b2d88b4b
z2trillion <z2trillion@users.noreply.github.com>
2019-08-15 12:28:00 -0500

    Add SparsePolynomial (#57)
    * Add DensePolynomial
    
    * Fix imports and comments
    
    * Add DensePolynomial
    
    * Fix imports and comments
    
    * Fix division
    
    * Add comment about DivAssign assumptions
    
    * Canonicalize DensePolynomials
    
    * Move evaluate to non-test

--- stark/src/polynomial.rs
+++ stark/src/polynomial.rs
@@ -3,12 +3,16 @@ use primefield::FieldElement;
 use rayon::prelude::*;
 use std::{
     cmp::max,
-    ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign},
+    collections::BTreeMap,
+    ops::{Add, AddAssign, DivAssign, Mul, MulAssign, Sub, SubAssign},
 };
-use u256::{commutative_binop, noncommutative_binop};
+use u256::{commutative_binop, noncommutative_binop, U256};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct DensePolynomial(Vec<FieldElement>);
+
+#[cfg_attr(test, derive(Debug, PartialEq, Clone))]
+pub struct SparsePolynomial(BTreeMap<usize, FieldElement>);
 
 impl DensePolynomial {
     // Coefficents are in order of ascending degree. E.g. &[1, 2] corresponds to the
@@ -32,6 +36,17 @@ impl DensePolynomial {
         }
         result
     }
+
+    // Removes trailing zeros or appends them so that the length is minimal and a
+    // power of two.
+    fn canonicalize(&mut self) {
+        let last_nonzero_index = match self.0.iter().enumerate().rev().find(|(_, x)| !x.is_zero()) {
+            Some((i, _)) => i,
+            None => 0,
+        };
+        let new_length = (last_nonzero_index + 1).next_power_of_two();
+        self.0.resize(new_length, FieldElement::ZERO);
+    }
 }
 
 // OPT: Write an Add<DensePolynomial> uses the fact that it's faster to add
@@ -46,7 +61,7 @@ impl AddAssign<&DensePolynomial> for DensePolynomial {
         if self.len() < other.len() {
             self.0.extend_from_slice(&other.0[self.len()..]);
         }
-        assert!(self.len().is_power_of_two());
+        self.canonicalize();
     }
 }
 
@@ -65,7 +80,7 @@ impl SubAssign<&Self> for DensePolynomial {
                 .map(|c| c.neg_assign())
                 .collect::<Vec<_>>();
         }
-        assert!(self.len().is_power_of_two());
+        self.canonicalize();
     }
 }
 
@@ -88,6 +103,7 @@ impl MulAssign<&Self> for DensePolynomial {
             .map(|(x, y)| *x *= y)
             .collect::<Vec<_>>();
         self.0 = ifft(&self.0);
+        self.canonicalize();
     }
 }
 
@@ -118,6 +134,89 @@ impl MulAssign<&FieldElement> for DensePolynomial {
     }
 }
 
+impl SparsePolynomial {
+    pub fn new(coefficients_and_degrees: &[(FieldElement, usize)]) -> Self {
+        let mut map = BTreeMap::new();
+        for (coefficient, degree) in coefficients_and_degrees {
+            assert!(!coefficient.is_zero());
+            match map.insert(*degree, coefficient.clone()) {
+                None => (),
+                Some(_) => panic!("Duplicate degrees found when constructing SparsePolynomial"),
+            };
+        }
+        assert!(!map.is_empty());
+        Self(map)
+    }
+
+    pub fn degree(&self) -> usize {
+        *self
+            .0
+            .iter()
+            .next_back()
+            .expect("SparsePolynomial cannot be empty")
+            .0
+    }
+
+    pub fn evaluate(&self, x: &FieldElement) -> FieldElement {
+        let mut result = FieldElement::ZERO;
+        for (degree, coefficient) in self.0.iter() {
+            result += coefficient * x.pow(U256::from(*degree));
+        }
+        result
+    }
+
+    fn leading_coefficient(&self) -> &FieldElement {
+        self.0
+            .iter()
+            .next_back()
+            .expect("SparsePolynomial cannot be empty")
+            .1
+    }
+}
+
+#[allow(clippy::suspicious_op_assign_impl)] // Allows us to use `+` here.
+impl MulAssign<SparsePolynomial> for DensePolynomial {
+    fn mul_assign(&mut self, other: SparsePolynomial) {
+        let mut result = vec![FieldElement::ZERO; self.len() + other.degree()];
+        for (degree, other_coefficient) in other.0.iter() {
+            for (i, self_coefficient) in self.0.iter().enumerate() {
+                result[i + degree] += self_coefficient * other_coefficient
+            }
+        }
+        self.0 = result;
+        self.canonicalize();
+    }
+}
+
+// This assumes that the sparse polynomial exactly divides the dense one, and
+// will panic if that is not the case.
+#[allow(clippy::suspicious_op_assign_impl)] // Allows us to use `*` here.
+impl DivAssign<SparsePolynomial> for DensePolynomial {
+    fn div_assign(&mut self, denominator: SparsePolynomial) {
+        if self.len() == 1 && self.0[0].is_zero() {
+            return;
+        }
+        let inverse_leading_coefficient = denominator
+            .leading_coefficient()
+            .inv()
+            .expect("SparsePolynomial has zero leading coefficient");
+        let denominator_degree = denominator.degree();
+        for i in (0..self.len()).rev() {
+            if i >= denominator_degree {
+                let quotient_coefficient = &self.0[i] * &inverse_leading_coefficient;
+                for (degree, coefficient) in denominator.0.iter() {
+                    self.0[i - denominator_degree + degree] -= &quotient_coefficient * coefficient;
+                }
+                self.0[i] = quotient_coefficient;
+            } else {
+                assert!(self.0[i].is_zero());
+            }
+        }
+        self.0.drain(0..denominator_degree);
+        self.canonicalize();
+    }
+}
+
 #[cfg(test)]
 use quickcheck::{Arbitrary, Gen};
 #[cfg(test)]
@@ -130,7 +229,25 @@ impl Arbitrary for DensePolynomial {
             length.next_power_of_two() - length
         ]);
         assert!(coefficients.len().is_power_of_two());
-        DensePolynomial(coefficients)
+        Self(coefficients)
+    }
+}
+#[cfg(test)]
+impl Arbitrary for SparsePolynomial {
+    fn arbitrary<G: Gen>(g: &mut G) -> Self {
+        let coefficients_and_degrees = Vec::<(FieldElement, usize)>::arbitrary(g);
+        let mut map = BTreeMap::new();
+        for (coefficient, degree) in coefficients_and_degrees {
+            if coefficient.is_zero() {
+                map.insert(degree, FieldElement::ONE);
+            } else {
+                map.insert(degree, coefficient);
+            }
+        }
+        if map.is_empty() {
+            map.insert(0, FieldElement::ONE);
+        }
+        Self(map)
     }
 }
 
@@ -140,18 +257,34 @@ mod tests {
     use quickcheck_macros::quickcheck;
 
     fn dense_polynomial(coefficients: &[isize]) -> DensePolynomial {
-        DensePolynomial(
+        let mut p = DensePolynomial(
             coefficients
                 .iter()
                 .map(|c| FieldElement::from(*c))
                 .collect(),
-        )
+        );
+        p.canonicalize();
+        p
+    }
+
+    fn sparse_polynomial(coefficients_and_degrees: &[(isize, usize)]) -> SparsePolynomial {
+        let v: Vec<_> = coefficients_and_degrees
+            .iter()
+            .map(|(c, d)| (FieldElement::from(*c), *d))
+            .collect();
+        SparsePolynomial::new(&v)
     }
 
     #[test]
     fn example_evaluate() {
         let p = dense_polynomial(&[1, 0, 0, 2]);
         assert_eq!(p.evaluate(&FieldElement::from(2)), FieldElement::from(17));
+    }
+
+    #[test]
+    fn example_sparse_evaluate() {
+        let p = sparse_polynomial(&[(1, 0), (1, 1)]);
+        assert_eq!(p.evaluate(&FieldElement::from(1)), FieldElement::from(2));
     }
 
     #[test]
@@ -173,6 +306,41 @@ mod tests {
         let p_1 = dense_polynomial(&[1, 2]);
         let p_2 = dense_polynomial(&[1, 2, 3, 4]);
         assert_eq!(p_1 * p_2, dense_polynomial(&[1, 4, 7, 10, 8, 0, 0, 0]));
+    }
+
+    #[test]
+    fn example_sparse_multiplication() {
+        let mut p = dense_polynomial(&[1, 2, 2, 0]);
+        p *= sparse_polynomial(&[(-1, 0), (1, 1)]);
+        assert_eq!(p, dense_polynomial(&[-1, -1, 0, 2]));
+    }
+
+    #[test]
+    fn example_division() {
+        let mut p = dense_polynomial(&[1, 3, 3, 1]);
+        p /= sparse_polynomial(&[(1, 0), (1, 1)]);
+        assert_eq!(p, dense_polynomial(&[1, 2, 1, 0]));
+    }
+
+    #[test]
+    fn example_division_1() {
+        let mut p = dense_polynomial(&[1, 1]);
+        p /= sparse_polynomial(&[(1, 0), (1, 1)]);
+        assert_eq!(p, dense_polynomial(&[1]));
+    }
+
+    #[test]
+    fn example_division_2() {
+        let mut p = dense_polynomial(&[-1, -1, 0, 2]);
+        p /= sparse_polynomial(&[(-1, 0), (1, 1)]);
+        assert_eq!(p, dense_polynomial(&[1, 2, 2, 0]));
+    }
+
+    #[test]
+    fn example_division_3() {
+        let mut p = dense_polynomial(&[1, 3, 3, 1]);
+        p /= sparse_polynomial(&[(1, 0), (1, 1)]);
+        assert_eq!(p, dense_polynomial(&[1, 2, 1, 0]));
     }
 
     #[test]
@@ -206,6 +374,21 @@ mod tests {
     }
 
     #[quickcheck]
+    fn sparse_product_evaluation_equivalence(
+        a: DensePolynomial,
+        b: SparsePolynomial,
+        x: FieldElement,
+    ) -> bool {
+        let evaluate_first = a.evaluate(&x) * b.evaluate(&x);
+
+        let mut product = a.clone();
+        product *= b;
+        let evaluate_last = product.evaluate(&x);
+
+        evaluate_first == evaluate_last
+    }
+
+    #[quickcheck]
     fn scalar_multiplication_evaluation_equivalence(
         a: DensePolynomial,
         c: FieldElement,
@@ -236,18 +419,23 @@ mod tests {
     }
 
     #[quickcheck]
-    fn addition_subtraction_inverse(
-        a: DensePolynomial,
-        b: DensePolynomial,
-        x: FieldElement,
-    ) -> bool {
+    fn addition_subtraction_inverse(a: DensePolynomial, b: DensePolynomial) -> bool {
         // We cannot directly check for equality of the two sides because adding and
         // subtracting b can change the length of a.
-        (&a + &b - b).evaluate(&x) == a.evaluate(&x)
+        &a + &b - b == a
     }
 
     #[quickcheck]
     fn distributivity(a: DensePolynomial, b: DensePolynomial, c: DensePolynomial) -> bool {
         &a * &b + &a * &c == a * (b + c)
+    }
+
+    #[quickcheck]
+    fn division_multiplication_inverse(a: DensePolynomial, b: SparsePolynomial) -> bool {
+        let mut test = a.clone();
+        test *= b.clone();
+        test /= b;
+        assert_eq!(a, test);
+        a == test
     }
 }
