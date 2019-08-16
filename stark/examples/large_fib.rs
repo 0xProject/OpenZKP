7dd762395cbf161c0d2a03af194551cdaa29e28a
Mason Liang <mason@0x.org>
2019-08-16 02:41:50 -0400

    Implement constraints using polynomials

--- stark/examples/large_fib.rs
+++ stark/examples/large_fib.rs
@@ -4,7 +4,7 @@ use macros_decl::u256h;
 use primefield::FieldElement;
 use stark::{
     check_proof,
-    fibonacci::{get_constraint, get_trace_table, PrivateInput, PublicInput},
+    fibonacci::{get_fibonacci_constraints, get_trace_table, PrivateInput, PublicInput},
     stark_proof, ProofParams,
 };
 use std::{env, time::Instant};
@@ -31,7 +31,8 @@ fn main() {
     let trace_table = get_trace_table(1_048_576, &private);
     public.value = trace_table[(public.index, 0)].clone();
     let start = Instant::now();
-    let potential_proof = stark_proof(&trace_table, &get_constraint(), &public, &ProofParams {
+    let constraints = get_fibonacci_constraints(&public);
+    let potential_proof = stark_proof(&trace_table, &constraints, &public, &ProofParams {
         blowup:                   16,
         pow_bits:                 12,
         queries:                  20,
@@ -45,7 +46,7 @@ fn main() {
 
     let verified = check_proof(
         potential_proof,
-        &get_constraint(),
+        &constraints,
         &public,
         &ProofParams {
             blowup:                   16,
