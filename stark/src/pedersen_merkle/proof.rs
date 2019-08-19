91a3676579f36601ad102333701cdd2eed7d48dc
z2trillion <z2trillion@users.noreply.github.com>
2019-08-19 10:23:17 -0400

    Add small pedersen merkle proof test (#75)

--- stark/src/pedersen_merkle/proof.rs
+++ stark/src/pedersen_merkle/proof.rs
@@ -3,7 +3,10 @@ mod tests {
     use crate::{
         pedersen_merkle::{
             constraints::get_pedersen_merkle_constraints,
-            inputs::{starkware_private_input, STARKWARE_PUBLIC_INPUT},
+            inputs::{
+                short_private_input, starkware_private_input, SHORT_PUBLIC_INPUT,
+                STARKWARE_PUBLIC_INPUT,
+            },
             trace_table::get_trace_table,
         },
         proofs::{stark_proof, ProofParams},
@@ -56,6 +59,42 @@ mod tests {
         assert_eq!(
             proof.proof[768..800],
             hex!("8af79c56d74b9252c3c542fc2b56d4692c608c98000000000000000000000000")
+        );
+    }
+
+    #[test]
+    fn short_pedersen_merkle() {
+        let public_input = SHORT_PUBLIC_INPUT;
+        let private_input = short_private_input();
+        let trace_table = get_trace_table(&public_input, &private_input);
+
+        let constraints = &get_pedersen_merkle_constraints(&public_input);
+
+        let proof = stark_proof(&trace_table, &constraints, &public_input, &ProofParams {
+            blowup:                   16,
+            pow_bits:                 0,
+            queries:                  13,
+            fri_layout:               vec![3, 2],
+            constraints_degree_bound: 2,
+        });
+
+        assert_eq!(
+            hex::encode(proof.proof[0..32].to_vec()),
+            "e2c4e35c37e33aa3b439592f2f3c5c82f464f026000000000000000000000000"
+        );
+        assert_eq!(
+            hex::encode(proof.proof[32..64].to_vec()),
+            "c5df989253ac4c3eff4fdb4130f832db1d2a9826000000000000000000000000"
+        );
+
+        // FRI commitments
+        assert_eq!(
+            hex::encode(proof.proof[640..672].to_vec()),
+            "744f04f8bcd9c5aafb8907586428fbe9dd81b976000000000000000000000000"
+        );
+        assert_eq!(
+            hex::encode(proof.proof[672..704].to_vec()),
+            "ce329839a5eccb8009ffebf029312989e68f1cde000000000000000000000000"
         );
     }
 }
