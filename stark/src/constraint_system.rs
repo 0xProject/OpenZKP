c482428dcd3b02ebb2cfa7938251725294f45c42
Remco Bloemen <remco@0x.org>
2019-09-04 14:39:30 -0700

    Add todos

--- stark/src/constraint_system.rs
+++ stark/src/constraint_system.rs
@@ -4,7 +4,12 @@ pub trait ConstraintSystem {
     type Public;
     type Private;
 
+    // TODO: This should return a `Result` with the `Error` type being associated
+    // type.
     fn constraints(public: &Self::Public) -> Constraints;
 
+    // TODO: This should return a `Result` with the `Error` type being associated
+    // type. TODO: The prover should check the trace table against the
+    // `Constraints` at least in debug mode.
     fn trace(public: &Self::Public, private: &Self::Private) -> TraceTable;
 }
