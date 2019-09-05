357d7cfaa1d4cc1b9446ac5336df9df1731acccd
Paul Vienhage <paul@0x.org>
2019-09-05 18:15:08 -0400

    Small naming things

--- substrate-runtime/src/exchange.rs
+++ substrate-runtime/src/exchange.rs
@@ -224,7 +224,7 @@ decl_module! {
 }
 
 // Hash of the zero vault ie hash(0, 0)
-pub const ZERO_VAULT: [u8; 32] =
+pub const EMPTY_VAULT_HASH: [u8; 32] =
     hex!("049ee3eba8c1600700ee1b87eb599f16716b0b1022947733551fde4050ca6804");
 
 impl<T: Trait> Module<T> {
@@ -243,7 +243,7 @@ impl<T: Trait> Module<T> {
     }
 
     fn empty_root(depth: u32) -> [u8; 32] {
-        let mut level = ZERO_VAULT;
+        let mut level = EMPTY_VAULT_HASH;
         for _ in 0..depth {
             level = hash(level, level);
         }
@@ -259,7 +259,7 @@ impl<T: Trait> Module<T> {
         let mut layer: Vec<[u8; 32]> = (0..power_of_two)
             .map(|x| {
                 if x > max_vault {
-                    ZERO_VAULT
+                    EMPTY_VAULT_HASH
                 } else {
                     <Vaults<T>>::get(x).vault_hash()
                 }
