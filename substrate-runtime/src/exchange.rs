d9dc15432c0cd181537ed35be850bf7850f78f7f
Paul Vienhage <paul@0x.org>
2019-09-09 14:33:17 -0400

    wip

--- substrate-runtime/src/exchange.rs
+++ substrate-runtime/src/exchange.rs
@@ -17,6 +17,8 @@ use u256::U256;
 /// The module's configuration trait.
 pub trait Trait: finality::Trait {}
 
+pub const SIZE_LIMIT = 1024; // The max number of trasactions we reasonably exepect in one proof
+
 // This module's storage items.
 decl_storage! {
     trait Store for Module<T: Trait> as Exchange {
@@ -28,6 +30,8 @@ decl_storage! {
         AvailableID get(get_available_id): u32 = 0;
         // Contains a listing of freed ids, and at key 0 a max id released which is known to always be free
         RecycledID get(get_recycled_id) build(|_config: &GenesisConfig<T>| {vec![(0_u32,0_u32)]}): map u32 => u32;
+        // Stores and holds the current number of transactions
+        BlockTransactions get(current_size) : u32 = 1024;
     }
 
     // Used for testing
