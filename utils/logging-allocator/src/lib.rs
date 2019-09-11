9518a82baf2a17b2ffa6a3d81a1a32510946905b
Remco Bloemen <remco@0x.org>
2019-09-11 12:23:29 -0700

    Finish logging-allocator

--- utils/logging-allocator/src/lib.rs
+++ utils/logging-allocator/src/lib.rs
@@ -1,34 +1,72 @@
+use log::{error, info, trace, warn};
 use std::{
     alloc::{GlobalAlloc, Layout, System},
+    ptr::null_mut,
+    sync::atomic::{AtomicUsize, Ordering::SeqCst},
+};
+
+pub struct LoggingAllocator {
+    info:      usize,
+    warn:      usize,
+    error:     usize,
+    reject:    usize,
+    allocated: AtomicUsize,
 }
-use log::{error, warn, info, trace};
 
-struct TracingAllocator;
-const INFO: usize = 1_000_000;
-const WARN: usize = 10_000_000
-const ERROR: usize = 100_000_000;
-const REJECT: usize = 1_000_000_000;
-
-unsafe impl GlobalAlloc for TracingAllocator {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        if layout.size() > INFO {
-            if layout.size() > ERROR {
-                error!("Allocating {:?} MB on heap", layout.size() / 1_000_000);
-                if layout.size() > REJECT {
-                    panic!("Rejecting allocation");
-                }
-            } else {
-                info!("Allocating {:?} MB on heap", layout.size() / 1_000_000);
-            }
+impl LoggingAllocator {
+    pub const fn new() -> Self {
+        Self {
+            info:      1_000_000,
+            warn:      10_000_000,
+            error:     100_000_000,
+            reject:    1_000_000_000,
+            allocated: AtomicUsize::new(0),
         }
-        System.alloc(layout)
+    }
+
+    pub fn allocated(&self) -> usize {
+        self.allocated.load(SeqCst)
+    }
+}
+
+unsafe impl GlobalAlloc for LoggingAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        if layout.size() < self.info {
+            trace!("Allocating {:?} MB on heap ({:?} MB allocated)",
+                layout.size() / 1_000_000,
+                self.allocated() / 1_000_000);
+        } else if layout.size() < self.warn {
+            info!("Allocating {:?} MB on heap ({:?} MB allocated)",
+                layout.size() / 1_000_000,
+                self.allocated() / 1_000_000);
+        } else if layout.size() < self.error {
+            warn!("Allocating {:?} MB on heap ({:?} MB allocated)",
+                layout.size() / 1_000_000,
+                self.allocated() / 1_000_000);
+        } else if layout.size() < self.reject {
+            error!("Allocating {:?} MB on heap ({:?} MB allocated)",
+                layout.size() / 1_000_000,
+                self.allocated() / 1_000_000);
+        } else {
+            error!(
+                "Rejecting {:?} MB allocation on heap ({:?} MB allocated)",
+                layout.size() / 1_000_000,
+                self.allocated() / 1_000_000);
+            return null_mut();
+        }
+        let result = System.alloc(layout);
+        if !result.is_null() {
+            self.allocated.fetch_add(layout.size(), SeqCst);
+        }
+        result
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        System.dealloc(ptr, layout)
+        System.dealloc(ptr, layout);
+        self.allocated.fetch_sub(layout.size(), SeqCst);
     }
 }
 
 #[cfg(enable)]
 #[global_allocator]
-static Allocator: TracingAllocator = TracingAllocator;
+pub static ALLOCATOR: LoggingAllocator = LoggingAllocator::new();
