7d70a67be71a273fdb5d26a5600a234e2764e682
Mason Liang <mason@0x.org>
2019-09-12 15:12:30 -0400

    wip

--- stark/src/constraint.rs
+++ stark/src/constraint.rs
@@ -23,31 +23,22 @@ pub fn combine_constraints(
     coefficients: &[FieldElement],
     trace_length: usize,
 ) -> RationalExpression {
-    let mut result = RationalExpression::from(0);
     let max_degree: usize = constraints
         .iter()
         .map(|c| c.degree(trace_length))
         .max()
         .unwrap();
-    let target_degree = max_degree.next_power_of_two();
-    for (i, constraint) in constraints.iter().enumerate() {
-        // these look good
-        // println!("numerator: {:?}", constraint.numerator.get_denominator());
-        // println!("denominator {:?}", constraint.denominator.get_denominator());
+    let result_degree = max_degree.next_power_of_two();
 
+    let mut result = RationalExpression::from(0);
+    for (i, constraint) in constraints.iter().enumerate() {
         let x =
             constraint.base.clone() * constraint.numerator.clone() / constraint.denominator.clone();
         let degree_adjustment = RationalExpression::X.pow(target_degree - x.degree(trace_length));
-        // println!("Constraint #{:?}!", i);
-        // if i < 2 {
-        //     assert_eq!(target_degree, 1024);
-        //     assert_eq!(x.degree(trace_length), 1);
-        // }
 
         result = result + Constant(coefficients[2 * i].clone()) * x.clone();
         result = result + Constant(coefficients[2 * i + 1].clone()) * x * degree_adjustment;
     }
-    // let mason: usize = result.degree(trace_length);
-    // assert_eq!(mason, trace_length);
+    debug_assert_eq!(result.degree(trace_length), result_degree);
     result
 }
