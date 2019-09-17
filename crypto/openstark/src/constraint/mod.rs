3a647062f7e199c73e2205f72675a7be9cd03e08
Mason Liang <mason@0x.org>
2019-09-17 12:07:10 -0400

    no_std_compat

--- crypto/openstark/src/constraint/mod.rs
+++ crypto/openstark/src/constraint/mod.rs
@@ -3,5 +3,7 @@ mod polynomial_expression;
 mod trace_expression;
 
 pub(crate) use constraint::{combine_constraints, Constraint};
-pub(crate) use polynomial_expression::PolynomialExpression::{Constant, PeriodicColumn, X};
+#[cfg(feature = "prover")]
+pub(crate) use polynomial_expression::PolynomialExpression::PeriodicColumn;
+pub(crate) use polynomial_expression::PolynomialExpression::{Constant, X};
 pub(crate) use trace_expression::TraceExpression::Trace;
--- crypto/openstark/src/polynomial.rs
+++ crypto/openstark/src/polynomial.rs
@@ -23,8 +23,7 @@ use u256::{commutative_binop, noncommutative_binop};
 pub struct DensePolynomial(Vec<FieldElement>);
 
 // TODO: Move into separate file or combine these into an enum.
-#[derive(PartialEq, Clone, Eq, PartialOrd, Ord)]
-#[cfg_attr(feature = "std", derive(Debug))]
+#[derive(PartialEq, Clone, Eq, PartialOrd, Ord, Debug)]
 pub struct SparsePolynomial(BTreeMap<usize, FieldElement>);
 
 impl DensePolynomial {
