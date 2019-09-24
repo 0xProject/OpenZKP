a173d633823de512015d547f0c20bda32b1b9054
Remco Bloemen <remco@0x.org>
2019-09-23 22:15:44 -0700

    Pretty print sizes

--- utils/logging-allocator/src/lib.rs
+++ utils/logging-allocator/src/lib.rs
@@ -42,39 +42,88 @@ use std::{
     alloc::{GlobalAlloc, Layout, System},
     ptr::null_mut,
     sync::atomic::{AtomicUsize, Ordering::SeqCst},
+    sync::atomic::Ordering,
 };
+use std::fmt;
+
+struct SizeBytes(usize);
+
+impl fmt::Display for SizeBytes {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let size = self.0 as f32;
+        if size < 1.0e3 {
+            write!(f, "{} B", size)
+        } else if size < 1.0e6 {
+            write!(f, "{:.1$} kB", size / 1.0e3, 5 - (size.log10().floor() as usize))
+        } else if size < 1.0e9 {
+            write!(f, "{:.1$} MB", size / 1.0e6, 8 - (size.log10().floor() as usize))
+        } else if size < 1.0e12 {
+            write!(f, "{:.1$} GB", size / 1.0e9, 11 - (size.log10().floor() as usize))
+        } else {
+            write!(f, "{} TB", size / 1.0e12)
+        }
+    }
+}
 
 // TODO: Make it store a static ref to an inner allocator (defaults to System)
 #[cfg_attr(feature = "std", derive(Debug))]
 pub struct LoggingAllocator {
-    info:      usize,
-    warn:      usize,
-    error:     usize,
-    reject:    usize,
-    allocated: AtomicUsize,
-    peak_allocated: AtomicUsize,
-    total_allocated: AtomicUsize,
-    num_allocations: AtomicUsize,
+    info:              usize,
+    warn:              usize,
+    error:             usize,
+    reject:            usize,
+    allocated:         AtomicUsize,
+    peak_allocated:    AtomicUsize,
+    total_allocated:   AtomicUsize,
+    largest_allocated: AtomicUsize,
+    num_allocations:   AtomicUsize,
 }
 
 impl LoggingAllocator {
+    // We need a `const fn` constructor
+    // TODO: Implement `Default` when trait functions can be `const`.
     pub const fn new() -> Self {
-        // TODO: impl Default
         Self {
-            info:      1_000_000,
-            warn:      10_000_000,
-            error:     100_000_000,
-            reject:    10_000_000_000,
-            allocated: AtomicUsize::new(0),
-            peak_allocated: AtomicUsize::new(0),
-            total_allocated: AtomicUsize::new(0),
+            info:              1_000_000,
+            warn:              10_000_000,
+            error:             100_000_000,
+            reject:            10_000_000_000,
+            allocated:         AtomicUsize::new(0),
+            peak_allocated:    AtomicUsize::new(0),
+            total_allocated:   AtomicUsize::new(0),
             largest_allocated: AtomicUsize::new(0),
-            num_allocations: AtomicUsize::new(0),
+            num_allocations:   AtomicUsize::new(0),
         }
     }
 
     pub fn allocated(&self) -> usize {
         self.allocated.load(SeqCst)
+    }
+
+    pub fn peak_allocated(&self) -> usize {
+        self.peak_allocated.load(SeqCst)
+    }
+
+    pub fn total_allocated(&self) -> usize {
+        self.total_allocated.load(SeqCst)
+    }
+
+    pub fn largest_allocated(&self) -> usize {
+        self.largest_allocated.load(SeqCst)
+    }
+
+    pub fn num_allocations(&self) -> usize {
+        self.num_allocations.load(SeqCst)
+    }
+
+    pub fn log_statistics(&self) {
+        info!(
+            "allocated {} in {} allocations. currently allocated {}, largest allocated {}",
+            SizeBytes(self.total_allocated()),
+            self.num_allocations(),
+            SizeBytes(self.allocated()),
+            SizeBytes(self.largest_allocated())
+        );
     }
 }
 
@@ -88,27 +137,27 @@ unsafe impl GlobalAlloc for LoggingAllocator {
             // allocations, and we want to prevent infinite recursion.
         } else if layout.size() < self.warn {
             info!(
-                "Allocating {:?} MB on heap ({:?} MB allocated)",
-                layout.size() / 1_000_000,
-                self.allocated() / 1_000_000
+                "Allocating {} on heap ({} allocated)",
+                SizeBytes(layout.size()),
+                SizeBytes(self.allocated())
             );
         } else if layout.size() < self.error {
             warn!(
-                "Allocating {:?} MB on heap ({:?} MB allocated)",
-                layout.size() / 1_000_000,
-                self.allocated() / 1_000_000
+                "Allocating {} on heap ({} allocated)",
+                SizeBytes(layout.size()),
+                SizeBytes(self.allocated())
             );
         } else if layout.size() < self.reject {
             error!(
-                "Allocating {:?} MB on heap ({:?} MB allocated)",
-                layout.size() / 1_000_000,
-                self.allocated() / 1_000_000
+                "Allocating {} on heap ({} allocated)",
+                SizeBytes(layout.size()),
+                SizeBytes(self.allocated())
             );
         } else {
             error!(
-                "Rejecting {:?} MB allocation on heap ({:?} MB allocated)",
-                layout.size() / 1_000_000,
-                self.allocated() / 1_000_000
+                "Rejecting {} allocation on heap ({} allocated)",
+                SizeBytes(layout.size()),
+                SizeBytes(self.allocated())
             );
             return null_mut();
         }
@@ -119,9 +168,9 @@ unsafe impl GlobalAlloc for LoggingAllocator {
             let allocated = self.allocated.fetch_add(layout.size(), SeqCst);
             let _ = self.total_allocated.fetch_add(layout.size(), SeqCst);
             let _ = self.num_allocations.fetch_add(1, SeqCst);
-            // TODO: Using `allocated` here again is not completely fool proof
-            max_cas_loop(&mut self.peak_allocated, allocated);
-            max_cas_loop(&mut self.largest_allocated, layout.size());
+            // HACK: Using `allocated` here again is not completely fool proof
+            let _ = max_cas_loop(&self.peak_allocated, allocated);
+            let _ = max_cas_loop(&self.largest_allocated, layout.size());
         }
         result
     }
@@ -132,16 +181,17 @@ unsafe impl GlobalAlloc for LoggingAllocator {
     }
 }
 
-// TODO: https://doc.rust-lang.org/std/sync/atomic/struct.AtomicUsize.html#method.fetch_max
+// TODO: Use `fetch_max` instead
+// https://doc.rust-lang.org/std/sync/atomic/struct.AtomicUsize.html#method.fetch_max
 // See also https://doc.rust-lang.org/src/core/sync/atomic.rs.html#1770-1783
-fn max_cas_loop(atom: &mut AtomicUsize, value: usize) -> usize {
-    const SET_ORDER: Ordering = Ordering::SeqCst;
-    const FETCH_ORDER: Ordering = Ordering::SeqCst;
-    let mut prev = atom.load(fetch_order);
+fn max_cas_loop(atom: &AtomicUsize, value: usize) -> usize {
+    const SET_ORDER: Ordering = SeqCst;
+    const FETCH_ORDER: Ordering = SeqCst;
+    let mut prev = atom.load(FETCH_ORDER);
     while prev < value {
-        match self.compare_exchange_weak(prev, value, SET_ORDER, FETCH_ORDER) {
+        match atom.compare_exchange_weak(prev, value, SET_ORDER, FETCH_ORDER) {
             Ok(_) => return value,
-            Err(next_prev) => prev = next_prev
+            Err(next_prev) => prev = next_prev,
         }
     }
     prev
