bc0f382d557843834dfb22570ea13d35d20df143
Paul Vienhage <paul@0x.org>
2019-09-26 18:38:40 -0400

    Working signature trace

--- crypto/openstark/src/group_sig/elliptic_helpers.rs
+++ crypto/openstark/src/group_sig/elliptic_helpers.rs
@@ -1,13 +1,15 @@
+use crate::TraceTable;
 use crate::{
     constraint_system::{Provable, Verifiable},
     constraints::Constraints,
     polynomial::DensePolynomial,
-    rational_expression::RationalExpression::{self, *},
-    TraceTable,
+    rational_expression::RationalExpression::*,
+    rational_expression::RationalExpression,
 };
-use macros_decl::{field_element, u256h};
-use primefield::FieldElement;
+use macros_decl::field_element;
+use primefield::{FieldElement};
 use u256::U256;
+use macros_decl::u256h;
 
 // Adjustment to ECDSA to make it unlikely to hit a zero.
 pub const SHIFT_POINT: (FieldElement, FieldElement) = (
@@ -16,8 +18,7 @@ pub const SHIFT_POINT: (FieldElement, FieldElement) = (
     )),
     FieldElement::from_montgomery(u256h!(
         "01211aac6ce572de4298f85b038ef6a8aeae324054290152c5c9927f66d85eeb"
-    )),
-);
+    )));
 
 pub const GENERATOR: (FieldElement, FieldElement) = (
     FieldElement::from_montgomery(u256h!(
@@ -25,164 +26,114 @@ pub const GENERATOR: (FieldElement, FieldElement) = (
     )),
     FieldElement::from_montgomery(u256h!(
         "05a0e71610f55329fbd89a97cf4b33ad0939e3442869bbe7569d0da34235308a"
-    )),
-);
+    )));
 
 // Elliptic curve param.
-const A: FieldElement = FieldElement::ONE;
+const A : FieldElement = FieldElement::ONE;
 
-// Provide this function with the input point and the output location
-// and it produces a constraint for point doubling first on the x output and one
-// one the y In default usage each input should be trace type.
-fn point_double(
-    input_x: RationalExpression,
-    input_y: RationalExpression,
-    output_x: RationalExpression,
-    output_y: RationalExpression,
-) -> [RationalExpression; 2] {
+// Provide this function with the input point and the output location 
+// and it produces a constraint for point doubling first on the x output and one one the y
+// In default usage each input should be trace type.
+fn point_double(input_x: RationalExpression, input_y: RationalExpression, output_x: RationalExpression, output_y: RationalExpression) -> [RationalExpression; 2] {
     let two = Constant(2.into());
     let three = Constant(3.into());
     let four = Constant(4.into());
     let eight = Constant(8.into());
-
-    // These constraints take the lambda = (3x_old^2 + a)/ 2(y_old) and multiply
-    // through to clear divisions. This is a multiplied through form of x_new =
-    // lambda^2 - 2x_old, which is asserted to be output_x
-    let lambda_numerator: RationalExpression =
-        three * Exp(input_x.clone().into(), 2) + Constant(A.into());
-    let new_x = Exp(lambda_numerator.clone().into(), 2)
-        - eight * Exp(input_y.clone().into(), 2) * input_x.clone()
-        - four * Exp(input_y.clone().into(), 2) * output_x.clone();
-    // This is a multipled through form of y_new = lambda*(x_old - x_new) - y_old,
-    // which is asserted to be output y.
-    let new_y = lambda_numerator * (input_x - output_x)
-        - two.clone() * Exp(input_y.clone().into(), 2)
-        - two * input_y * output_y;
+    
+    // These constraints take the lambda = (3x_old^2 + a)/ 2(y_old) and multiply through to clear divisions.
+    // This is a multiplied through form of x_new = lambda^2 - 2x_old, which is asserted to be output_x
+    let lambda_numerator : RationalExpression = three*Exp(input_x.clone().into(), 2) + Constant(A.into());
+    let new_x = Exp(lambda_numerator.clone().into(), 2) - eight*Exp(input_y.clone().into(), 2)*input_x.clone() - four*Exp(input_y.clone().into(), 2)*output_x.clone();
+    // This is a multipled through form of y_new = lambda*(x_old - x_new) - y_old, which is asserted to be output y.
+    let new_y = lambda_numerator*(input_x - output_x) - two.clone()*Exp(input_y.clone().into(), 2) - two*input_y*output_y;
     [new_x, new_y]
 }
 
-// Provide this function the two points P and Q to add plus the asserted output
-// location It provides constraints that express that Out = P + Q
-fn point_add(
-    x_p: RationalExpression,
-    y_p: RationalExpression,
-    x_q: RationalExpression,
-    y_q: RationalExpression,
-    x_out: RationalExpression,
-    y_out: RationalExpression,
-) -> [RationalExpression; 2] {
-    // These constraints take the lambda = (y_q - y_p)/ (x_q - x_p) and multiply
-    // through to clear divisions.
+// Provide this function the two points P and Q to add plus the asserted output location
+// It provides constraints that express that Out = P + Q
+fn point_add(x_p: RationalExpression, y_p: RationalExpression, x_q: RationalExpression, y_q: RationalExpression, x_out: RationalExpression, y_out: RationalExpression) -> [RationalExpression; 2] {
+    // These constraints take the lambda = (y_q - y_p)/ (x_q - x_p) and multiply through to clear divisions.
     let lambda_numerator = y_q.clone() - y_p.clone();
     let lambda_denominator = x_q.clone() - x_p.clone();
-    let new_x = Exp(lambda_numerator.clone().into(), 2)
-        - lambda_denominator.clone() * (x_p.clone() - x_q.clone() - x_out.clone());
-    let new_y = lambda_numerator * (x_p - x_out) - lambda_denominator * (y_p + y_out);
+    let new_x = Exp(lambda_numerator.clone().into(), 2) - lambda_denominator.clone()*(x_p.clone() - x_q.clone() - x_out.clone());
+    let new_y = lambda_numerator*(x_p - x_out) - lambda_denominator*(y_p + y_out);
     [new_x, new_y]
 }
 
-// Full conditional bool check that location is a if test and b if !test
-// [secured by the check that test = 1 or 0]
-fn conditional(
-    a: RationalExpression,
-    b: RationalExpression,
-    location: RationalExpression,
-    test: RationalExpression,
-) -> [RationalExpression; 2] {
-    [
-        one_or_zero(test.clone()),
-        simple_conditional(a, b, location, test),
-    ]
+// Full conditional bool check that location is a if test and b if !test [secured by the check that test = 1 or 0]
+fn conditional(a: RationalExpression, b: RationalExpression, location: RationalExpression, test: RationalExpression) -> [RationalExpression; 2] {
+    [one_or_zero(test.clone()), simple_conditional(a, b, location, test)]
 }
 
 // Tests if a rational expression is not one or zero
 fn one_or_zero(test: RationalExpression) -> RationalExpression {
-    test.clone() * (Constant(FieldElement::ONE) - test.clone())
+    test.clone()*(Constant(FieldElement::ONE) - test.clone())
 }
 
 // Non secured conditional check.
-fn simple_conditional(
-    a: RationalExpression,
-    b: RationalExpression,
-    location: RationalExpression,
-    test: RationalExpression,
-) -> RationalExpression {
-    a * test.clone() + (Constant(FieldElement::ONE) - test.clone()) * b - location.clone()
+fn simple_conditional(a: RationalExpression, b: RationalExpression, location: RationalExpression, test: RationalExpression) -> RationalExpression {
+    a*test.clone() + (Constant(FieldElement::ONE) - test.clone())*b - location.clone()
 }
 
-// This function takes in a target and a claimed bit decomposition vector and
-// returns constraints which check that (1) the decomp is all ones and zeros and
-// (2) that the target equals the sum of increasing powers of two of the decomp.
-// Note given the size limit of field elements we expect that decomp is len <
-// 256
-fn bit_decomp_test(
-    target: RationalExpression,
-    decomp: Vec<RationalExpression>,
-) -> [RationalExpression; 2] {
+// This function takes in a target and a claimed bit decomposition vector and returns constraints which check that
+// (1) the decomp is all ones and zeros and (2) that the target equals the sum of increasing powers of two of the decomp.
+// Note given the size limit of field elements we expect that decomp is len < 256
+fn bit_decomp_test(target: RationalExpression, decomp: Vec<RationalExpression>) -> [RationalExpression; 2] {
     let mut power = FieldElement::ONE;
     let mut consistency = Constant(FieldElement::ZERO);
     let mut sum = Constant(FieldElement::ZERO);
 
     for bit in decomp.iter() {
         consistency = consistency + one_or_zero(bit.clone());
-        sum = sum + bit.clone() * Constant(power.clone());
-        power *= FieldElement::from(2);
+        sum = sum + bit.clone()*Constant(power.clone());
+        power  *= FieldElement::from(2);
     }
     [consistency, sum - target]
 }
 
-// We make the assumption that zero will never be placed into N then add a col
-// Q+N without knowing if it is right
-pub fn scalar_mult(
-    trace: &mut TraceTable,
-    point: (FieldElement, FieldElement),
-    scalar: &FieldElement,
-    start: usize,
-    offset: usize,
-) {
+// We make the assumption that zero will never be placed into N then add a col Q+N without knowing if it is right
+pub fn scalar_mult(trace: &mut TraceTable, point: (FieldElement, FieldElement), scalar: &U256, start: usize, offset: usize, neg: bool)  {
     // Add an extra copy of the point then set q to it's negative.
     let mut n = point.clone();
-    let mut q = SHIFT_POINT.clone();
+    let mut q;
+    if neg {
+        q = (SHIFT_POINT.0, -&SHIFT_POINT.1);
+    } else {
+        q = SHIFT_POINT.clone();
+    }
 
-    // Takes non montgomery form from the scalar and divides up it's bits
-    let bit_accurate = U256::from(scalar);
     for i in 0..256 {
-        if bit_accurate.bit(i) {
+        if scalar.bit(i) {
             trace[(start + i, offset)] = FieldElement::ONE;
             q = add(&q.0, &q.1, &n.0, &n.1);
         } else {
             trace[(start + i, offset)] = FieldElement::ZERO;
         }
         n = double(&n.0, &n.1);
-
-        trace[(start + i, offset + 1)] = n.0.clone();
-        trace[(start + i, offset + 2)] = n.1.clone();
-        trace[(start + i, offset + 3)] = q.0.clone();
-        trace[(start + i, offset + 4)] = q.1.clone();
+        
+        trace[(start+i, offset + 1)] = n.0.clone();
+        trace[(start+i, offset + 2)] = n.1.clone();
+        trace[(start+i, offset + 3)] = q.0.clone();
+        trace[(start+i, offset + 4)] = q.1.clone();
     }
 }
 
 // Note incorrect when given zero inputs
 pub fn double(point_x: &FieldElement, point_y: &FieldElement) -> (FieldElement, FieldElement) {
-    assert!(!(point_x == &FieldElement::ZERO && point_y == &FieldElement::ZERO));
-    let lambda = ((point_x + point_x + point_x) * point_x + A) / (point_y + point_y);
+    assert!(!(point_x == &FieldElement::ZERO && point_y == &FieldElement::ZERO ));
+    let lambda = ((point_x + point_x + point_x)*point_x + A)/(point_y + point_y);
     let new_x = (&lambda).square() - point_x - point_x;
-    let new_y = lambda * (point_x - &new_x) - point_y;
+    let new_y = lambda*(point_x - &new_x) - point_y;
     (new_x, new_y)
 }
 
 // Note incorrect when given zero inputs
-pub fn add(
-    x_p: &FieldElement,
-    y_p: &FieldElement,
-    x_q: &FieldElement,
-    y_q: &FieldElement,
-) -> (FieldElement, FieldElement) {
+pub fn add(x_p: &FieldElement, y_p: &FieldElement, x_q: &FieldElement, y_q: &FieldElement) -> (FieldElement, FieldElement) {
     assert!(!(x_p == &FieldElement::ZERO && y_p == &FieldElement::ZERO));
     assert!(!(x_q == &FieldElement::ZERO && y_q == &FieldElement::ZERO));
-    let lambda = (y_q - y_p) / (x_q - x_p);
+    let lambda = (y_q - y_p)/(x_q - x_p);
     let new_x = (&lambda).square() - (x_p + x_q);
-    let new_y = lambda * (x_p - &new_x) - y_p;
+    let new_y = lambda*(x_p - &new_x) - y_p;
     (new_x, new_y)
 }
 
@@ -203,18 +154,15 @@ mod test {
         );
         let c = u256h!("07374b7d69dc9825fc758b28913c8d2a27be5e7c32412f612b20c9c97afbe4dd");
 
-        let expected_affine = Affine::Point {
-            x: p.0.clone(),
-            y: p.1.clone(),
-        } * &c;
-        let mut expected: (FieldElement, FieldElement) = match expected_affine {
-            Affine::Point { x, y } => (x, y),
+        let expected_affine = Affine::Point{x: p.0.clone(), y: p.1.clone()} * &c;
+        let mut expected : (FieldElement, FieldElement) = match expected_affine {
+            Affine::Point {x, y} => (x, y),
             _ => (FieldElement::ZERO, FieldElement::ZERO),
         };
         expected = add(&expected.0, &expected.1, &SHIFT_POINT.0, &SHIFT_POINT.1);
 
         let mut trace = TraceTable::new(256, 5);
-        scalar_mult(&mut trace, (p.0, p.1), &FieldElement::from(c), 0, 0);
+        scalar_mult(&mut trace, (p.0, p.1), &c, 0, 0, false);
         assert_eq!(trace[(255, 3)], expected.0);
         assert_eq!(trace[(255, 4)], expected.1);
     }
--- crypto/openstark/src/group_sig/signature.rs
+++ crypto/openstark/src/group_sig/signature.rs
@@ -10,6 +10,7 @@ use crate::{
 use macros_decl::field_element;
 use primefield::{fft::ifft, FieldElement};
 use u256::U256;
+use elliptic_curve_crypto as ecc;
 
 struct Claim {
     hash: FieldElement,
@@ -49,13 +50,13 @@ impl Provable<Claim> for Witness {
     fn trace(&self, claim: &Claim) -> TraceTable {
         let mut trace = TraceTable::new(512, 5);
         // u_1 = hash * s inverse
-        let u_1 = &claim.hash * &self.signature.1;
+        let u_1 = U256::from(&claim.hash).mulmod(&U256::from(&self.signature.1), &elliptic_curve::ORDER);
         // u_2 = r * s inverse
-        let u_2 = &self.signature.0 * &self.signature.1;
+        let u_2 = U256::from(&self.signature.0).mulmod(&U256::from(&self.signature.1), &elliptic_curve::ORDER);
         // u_1 x G
-        scalar_mult(&mut trace, GENERATOR.clone(), &u_1, 0, 0);
+        scalar_mult(&mut trace, GENERATOR.clone(), &u_1, 0, 0, false);
         // u_2 x PublicKey
-        scalar_mult(&mut trace, claim.who.clone(), &u_2, 256, 0);
+        scalar_mult(&mut trace, claim.who.clone(), &u_2, 256, 0, true);
         trace
     }
 }
@@ -64,15 +65,13 @@ impl Provable<Claim> for Witness {
 mod test {
     use super::*;
     use elliptic_curve::Affine;
-    use elliptic_curve_crypto as ecc;
     use macros_decl::{field_element, u256h};
 
     #[test]
     fn test_sign_table() {
         let private_key =
             u256h!("03c1e9550e66958296d11b60f8e8e7a7ad990d07fa65d5f7652c4a6c87d4e3cc");
-        let message_hash =
-            FieldElement::from(u256h!("01921ce52df68f0185ade7572776513304bdd4a07faf6cf28cefc65a86fc496c"));
+        let message_hash = u256h!("01921ce52df68f0185ade7572776513304bdd4a07faf6cf28cefc65a86fc496c");
         let public_affine = ecc::private_to_public(&private_key);
         let public = match public_affine.clone() {
             Affine::Zero => (FieldElement::ZERO, FieldElement::ZERO),
@@ -81,11 +80,11 @@ mod test {
 
         let (r, w) = ecc::sign(&U256::from(message_hash.clone()), &private_key);
 
-        let u_1 = message_hash.clone() * FieldElement::from(&w);
-        let u_2 = FieldElement::from(&r) * FieldElement::from(&w);
+        let u_1 = message_hash.mulmod(&w, &elliptic_curve::ORDER);
+        let u_2 = &r.mulmod(&w, &elliptic_curve::ORDER);
 
-        let first_expected_affine = elliptic_curve::GENERATOR * &U256::from(u_1);
-        let second_expected_affine = public_affine * &U256::from(u_2);
+        let first_expected_affine = elliptic_curve::GENERATOR * u_1;
+        let second_expected_affine = public_affine * u_2;
         let first_expected = match first_expected_affine.clone() {
             Affine::Zero => (FieldElement::ZERO, FieldElement::ZERO),
             Affine::Point { x, y } => (x, y),
@@ -116,8 +115,8 @@ mod test {
         let shifted_trace2 = add(
             &trace[(511, 3)],
             &trace[(511, 4)],
-            &neg_shift.0,
-            &neg_shift.1,
+            &SHIFT_POINT.0,
+            &SHIFT_POINT.1,
         );
         assert_eq!(first_expected, shifted_trace1.clone());
         assert_eq!(second_expected, shifted_trace2.clone());
@@ -128,28 +127,6 @@ mod test {
             &trace[(511, 3)],
             &trace[(511, 4)],
         );
-        let double_neg_shift = double(&neg_shift.0, &neg_shift.1);
-        assert_eq!(
-            add(
-                &final_check.0,
-                &final_check.1,
-                &double_neg_shift.0,
-                &double_neg_shift.1
-            ),
-            add(
-                &shifted_trace1.0,
-                &shifted_trace1.1,
-                &shifted_trace2.0,
-                &shifted_trace2.1
-            )
-        );
-        final_check = add(
-            &final_check.0,
-            &final_check.1,
-            &double_neg_shift.0,
-            &double_neg_shift.1,
-        );
-
         assert_eq!(FieldElement::from(r), final_check.0);
     }
 }
