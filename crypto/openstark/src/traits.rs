127215669543035486aa329ced4fd712bbc84c39
Mason Liang <mason@0x.org>
2019-10-04 16:03:40 -0700

    Move seed into Claim

--- crypto/openstark/src/traits.rs
+++ crypto/openstark/src/traits.rs
@@ -29,6 +29,13 @@ pub(crate) mod tests {
     use primefield::FieldElement;
     use quickcheck::{Arbitrary, Gen};
 
+    /// Helper struct for
+    #[derive(Clone, PartialEq, Debug)]
+    pub(crate) struct Recurrance {
+        pub(crate) index:         usize,
+        pub(crate) initial_value: FieldElement,
+    }
+
     #[derive(Clone, PartialEq, Debug)]
     pub(crate) struct Claim {
         index: usize,
@@ -40,21 +47,7 @@ pub(crate) mod tests {
         secret: FieldElement,
     }
 
-    #[derive(Clone, PartialEq, Debug)]
-    pub(crate) struct Recurrance {
-        pub(crate) index:         usize,
-        pub(crate) initial_value: FieldElement,
-    }
-
     impl Recurrance {
-        pub(crate) fn index_value(&self) -> FieldElement {
-            let mut state = (FieldElement::ONE, self.initial_value.clone());
-            for _ in 0..self.index {
-                state = (state.1.clone(), state.0 + state.1);
-            }
-            state.0
-        }
-
         pub(crate) fn claim(&self) -> Claim {
             Claim {
                 index: self.index,
@@ -67,11 +60,28 @@ pub(crate) mod tests {
                 secret: self.initial_value.clone(),
             }
         }
+
+        fn index_value(&self) -> FieldElement {
+            let mut state = (FieldElement::ONE, self.initial_value.clone());
+            for _ in 0..self.index {
+                state = (state.1.clone(), state.0 + state.1);
+            }
+            state.0
+        }
+    }
+
+    impl Claim {
+        pub(crate) fn seed(&self) -> Vec<u8> {
+            let mut seed = self.index.to_be_bytes().to_vec();
+            seed.extend_from_slice(&self.value.as_montgomery().to_bytes_be());
+            seed
+        }
     }
 
     impl Arbitrary for Recurrance {
         fn arbitrary<G: Gen>(g: &mut G) -> Self {
             Recurrance {
+                // TODO: handle 1 row trace tables.
                 index:         1 + usize::arbitrary(g),
                 initial_value: FieldElement::arbitrary(g),
             }
@@ -82,10 +92,6 @@ pub(crate) mod tests {
         fn constraints(&self) -> Constraints {
             use RationalExpression::*;
 
-            // Seed
-            let mut seed = self.index.to_be_bytes().to_vec();
-            seed.extend_from_slice(&self.value.as_montgomery().to_bytes_be());
-
             // Constraint repetitions
             let trace_length = (self.index + 1).next_power_of_two();
             let trace_generator = FieldElement::root(trace_length).unwrap();
@@ -94,7 +100,7 @@ pub(crate) mod tests {
             let every_row = || (X - g.pow(trace_length - 1)) / (X.pow(trace_length) - 1.into());
 
             // Constraints
-            Constraints::from_expressions((trace_length, 2), seed, vec![
+            Constraints::from_expressions((trace_length, 2), self.seed(), vec![
                 (Trace(0, 1) - Trace(1, 0)) * every_row(),
                 (Trace(1, 1) - Trace(0, 0) - Trace(1, 0)) * every_row(),
                 (Trace(0, 0) - 1.into()) * on_row(0),
