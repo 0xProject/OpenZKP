aeb01a20550cc570f593dba1be2701aa8c6aea0d
Remco Bloemen <remco@0x.org>
2019-10-07 18:46:30 +0900

    Re-export primefield

--- algebra/primefield/src/lib.rs
+++ algebra/primefield/src/lib.rs
@@ -48,6 +48,9 @@ pub use field::FieldElement;
 // TODO: Make member functions of FieldElement?
 pub use field::{invert_batch, invert_batch_src_dst};
 
+// Re-exports for convenience
+pub use zkp_u256 as u256;
+
 // Std/no-std imports
 #[cfg(not(feature = "std"))]
 extern crate no_std_compat as std;
