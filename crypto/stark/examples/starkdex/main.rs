0103323125fd379119d641745d974bb65a1b125e
z2trillion <z2trillion@users.noreply.github.com>
2019-10-30 14:44:47 -0400

    Mason/test/starkdex (#404)
    * Only one error now
    
    * borrow checking time
    
    * builds
    
    * Start split into smaller functions
    
    * Move signature verification out
    
    * start state_transistion
    
    * Move state_transition out
    
    * move hash_pool out
    
    * simplify naming
    
    * Remove state_transition__merkle_update__
    
    * Simply names
    
    * nest functions
    
    * move functions around
    
    * simplify comment
    
    * remove solidity file
    
    * Fix warnings
    
    * Simplify to 1.into() where possible
    
    * remove double clones
    
    * skip formating for constraints
    
    * fix skip

---
+++ crypto/stark/examples/starkdex/main.rs
@@ -0,0 +1,21 @@
+#[rustfmt::skip] // For now, code is easier to grep unformated.
+mod constraints;
+// mod inputs;
+// mod periodic_columns;
+// mod trace_table;
+
+use env_logger;
+// use log::info;
+// use std::time::Instant;
+// use zkp_macros_decl::{field_element, hex};
+// use zkp_primefield::FieldElement;
+// use zkp_stark::{prove, Provable};
+// use zkp_u256::U256;
+
+// Need to import to active the logging allocator
+#[allow(unused_imports)]
+use zkp_logging_allocator;
+
+fn main() {
+    env_logger::init();
+}
