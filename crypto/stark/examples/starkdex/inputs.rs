a8dcf3bebc673146fee71bc96839226f65979227
Mason Liang <mason@0x.org>
2019-11-07 18:10:25 -0500

    wip

--- crypto/stark/examples/starkdex/inputs.rs
+++ crypto/stark/examples/starkdex/inputs.rs
@@ -70,18 +70,33 @@ pub struct Vaults {
     vaults: BTreeMap<u32, Vault>,
 }
 
+#[derive(PartialEq, Clone, PartialOrd, Eq, Ord)]
+#[cfg_attr(feature = "std", derive(Debug))]
+enum Direction {
+    LEFT,
+    RIGHT,
+}
+
+impl Direction {
+    pub fn other(&self) -> Self {
+        match self {
+            Self::LEFT => Self::RIGHT,
+            Self::RIGHT => Self::LEFT,
+        }
+    }
+}
+
 #[derive(PartialEq, Clone)]
 #[cfg_attr(feature = "std", derive(Debug))]
 pub struct Tree {
-    height: usize,
-    hash:   FieldElement,
-    left:   Option<Box<Tree>>,
-    right:  Option<Box<Tree>>,
+    height:   usize,
+    hash:     FieldElement,
+    children: BTreeMap<Direction, Tree>,
 }
 
 impl Vault {
     pub fn hash(&self) -> FieldElement {
-        FieldElement::ZERO
+        hash(&self.key, &self.token)
     }
 }
 
@@ -105,20 +120,23 @@ impl Tree {
         Self {
             height,
             hash: Self::empty_hash(height),
-            left: None,
-            right: None,
+            children: BTreeMap::new(),
         }
     }
 
     pub fn path(&self, index: u32) -> Vec<FieldElement> {
         match self.height {
-            0 => vec![self.hash.clone()],
+            0 => vec![],
             _ => {
-                let mut result = vec![self.hash.clone()];
-                match self.subtree(index) {
-                    None => result.extend(self.empty_hashes()),
-                    Some(t) => result.extend(t.path(index)),
+                let direction = self.direction(index);
+                let mut result = match self.children.get(&direction) {
+                    None => self.empty_hashes(),
+                    Some(t) => t.path(index),
                 };
+                result.push( match self.children.get(&direction.other()) {
+                    None => Self::empty_hash(self.height - 1),
+                    Some(t) => t.hash.clone(),
+                } );
                 result
             }
         }
@@ -129,21 +147,22 @@ impl Tree {
             0 => self.hash = vault.hash(),
             _ => {
                 let height = self.height;
-                let next = self.mut_subtree(index);
+                let direction = self.direction(index);
+                let next = self.children.get_mut(&direction);
                 match next {
                     None => {
                         let mut t = Tree::new(height - 1);
                         t.update(index, vault);
-                        *next = Some(Box::new(t));
+                        self.children.insert(direction, t);
                     }
                     Some(t) => t.update(index, vault),
                 };
 
-                let left_hash = match &self.left {
+                let left_hash = match self.children.get(&Direction::LEFT) {
                     None => Self::empty_hash(height - 1),
                     Some(t) => t.hash.clone(),
                 };
-                let right_hash = match &self.right {
+                let right_hash = match self.children.get(&Direction::RIGHT) {
                     None => Self::empty_hash(height - 1),
                     Some(t) => t.hash.clone(),
                 };
@@ -152,19 +171,11 @@ impl Tree {
         };
     }
 
-    fn mut_subtree(&mut self, index: u32) -> &mut Option<Box<Tree>> {
+    fn direction(&self, index: u32) -> Direction {
         if index & (1 << self.height) != 0 {
-            &mut self.left
+            Direction::LEFT
         } else {
-            &mut self.right
-        }
-    }
-
-    fn subtree(&self, index: u32) -> &Option<Box<Tree>> {
-        if index & (1 << self.height) != 0 {
-            &self.left
-        } else {
-            &self.right
+            Direction::RIGHT
         }
     }
 
@@ -177,16 +188,73 @@ impl Tree {
     }
 
     fn empty_hashes(&self) -> Vec<FieldElement> {
-        assert!(self.left.is_none() || self.right.is_none());
-        (0..self.height).map(Self::empty_hash).collect()
+        (1..self.height-1).map(Self::empty_hash).collect()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use zkp_macros_decl::field_element;
     use zkp_primefield::FieldElement;
     use zkp_u256::U256;
+
+    fn get_directions(index: usize) -> Vec<Direction> {
+        let mut index = index;
+        let mut directions = vec![];
+        for _ in 0..4 {
+            directions.push(if index % 2 == 0 {
+                Direction::LEFT
+            } else {
+                Direction::RIGHT
+            });
+            index /= 2;
+        }
+        directions
+    }
+
+    fn root(
+        leaf_hash: &FieldElement,
+        directions: &[Direction],
+        path: &[FieldElement],
+    ) -> FieldElement {
+        let mut root = leaf_hash.clone();
+        for (direction, sibling_hash) in directions.iter().zip(path) {
+            assert_eq!(sibling_hash.clone(), root);
+            root = match direction {
+                Direction::LEFT => hash(&root, &sibling_hash),
+                Direction::RIGHT => hash(&sibling_hash, &root),
+            };
+        }
+        root
+    }
+
+    #[test]
+    fn path_correct() {
+        let mut tree = Tree::new(4);
+        let vault = Vault {
+            key:    FieldElement::ZERO,
+            token:  FieldElement::ZERO,
+            amount: 1000,
+        };
+        // tree.update(5, vault.clone());
+        // dbg!(tree.path(5));
+        // dbg!(tree.path(0));
+        // dbg!(tree.path(1));
+
+        let directions = &get_directions(5);
+        let path = tree.path(5);
+        dbg!(directions.clone());
+        dbg!(path.clone());
+        assert_eq!(root(&vault.hash(), &directions, &path), tree.hash);
+        // assert!(false);
+    }
+
+    #[test]
+    fn hash_test() {
+        let x = field_element!("05d702904f10e78036abca2229077576488062ef2a9b44eb16b9e12a07932764");
+        assert_eq!(hash(&x, &x), field_element!("04eb5413fc27c3950f8e9d3bd9ba325b6fcc144f6f484e10ebb6b6fda9b642a9"));
+    }
 
     #[test]
     fn test_is_settlement_0() {
