2840f53e55fd4407b19e644435336171ac5f7dc0
Mason Liang <mason@0x.org>
2019-11-12 17:52:15 -0800

    16

--- crypto/stark/examples/starkdex/constraints.rs
+++ crypto/stark/examples/starkdex/constraints.rs
@@ -342,7 +342,7 @@ mod tests {
         let trace = |i: usize, j: isize| trace_values.get(&(i, j)).unwrap().clone();
 
         let mut coefficients = vec![FieldElement::ZERO; 2 * 120];
-        for i in 0..8 {
+        for i in 0..16 {
             coefficients[i] = FieldElement::ONE;
         }
 
