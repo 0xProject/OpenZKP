b09d8b7ab5f17df66c05304828bd57b289aba55c
Remco Bloemen <remco@0x.org>
2019-12-18 17:36:00 -0800

    Field trait

--- algebra/primefield/src/const_generics.rs
+++ algebra/primefield/src/const_generics.rs
@@ -1,58 +1,75 @@
 use zkp_u256::{U256};
 use std::marker::PhantomData;
 use std::ops::Add;
+use std::fmt;
 
-pub trait FieldParameters {
+pub trait Field: Sized + fmt::Debug + Add<Output = Self> {
+    const MODULUS: U256;
+    const ZERO: Self;
+    const ONE: Self;
+
+    fn from_u256(value: U256) -> Self;
+}
+
+pub trait PrimeFieldParameters {
     const MODULUS: U256;
 }
 
-pub struct Element<F: FieldParameters> {
+pub struct PrimeField<F: PrimeFieldParameters> {
     value: U256,
     _field: PhantomData<F>,
 }
 
-impl<F: FieldParameters> Element<F> {
-    pub const MODULUS: U256 = F::MODULUS;
-    pub const ZERO: Self = Element { value: U256::ZERO, _field: PhantomData };
-    pub const ONE: Self = Element { value: U256::ONE, _field: PhantomData };
-    
-    pub fn from_u256(mut value: U256) -> Self {
+impl<F: PrimeFieldParameters> Field for PrimeField<F> {
+    const MODULUS: U256 = F::MODULUS;
+    const ZERO: Self = PrimeField { value: U256::ZERO, _field: PhantomData };
+    const ONE: Self = PrimeField { value: U256::ONE, _field: PhantomData };
+
+    fn from_u256(mut value: U256) -> Self {
         value %= Self::MODULUS;
-        Element { value, _field: PhantomData }
+        PrimeField { value, _field: PhantomData }
     }
 }
 
-impl<F: FieldParameters> Add for &Element<F> {
-    type Output = Element<F>;
+impl<F: PrimeFieldParameters> fmt::Debug for PrimeField<F> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self.value)
+    }
+}
+
+impl<F: PrimeFieldParameters> Add for PrimeField<F> {
+    type Output = PrimeField<F>;
     fn add(self, other: Self) -> Self::Output {
         // TODO: Overflow
-        Element::from_u256(self.value.clone() + &other.value)
+        PrimeField::from_u256(self.value.clone() + &other.value)
     }
 }
-
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use zkp_macros_decl::u256h;
 
-    struct SmallFieldParameters();
-
-    impl FieldParameters for SmallFieldParameters {
-        // 11
-        const MODULUS: U256 = u256h!("000000000000000000000000000000000000000000000000000000000000000a");
+    // Function is completely abstract over the field implementation.
+    fn generic_test<F: Field>() {
+        let a = F::from_u256(U256::from(5));
+        let b = F::from_u256(U256::from(234234323));
+        let c = a + b;
+        dbg!(&c);
+        let c = c + F::ONE;
+        dbg!(&c);
     }
-
-    type F = Element<SmallFieldParameters>;
 
     #[test]
     fn test_element() {
-        let a = F::from_u256(U256::from(5));
-        let b = F::from_u256(U256::from(234234323));
-        let c = &a + &b;
-        dbg!(&c.value);
-        let c = &c + &F::ONE;
-        dbg!(&c.value);
+        // Create a type F that implements a Prime Field 
+        struct SmallFieldParameters();
+        impl PrimeFieldParameters for SmallFieldParameters {
+            // 11
+            const MODULUS: U256 = u256h!("000000000000000000000000000000000000000000000000000000000000000a");
+        }
+        type SmallField = PrimeField<SmallFieldParameters>;
+
+        generic_test::<SmallField>();
     }
 }
