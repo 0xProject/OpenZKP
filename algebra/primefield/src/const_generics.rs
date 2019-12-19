7b93e3dd5f8e77ad196888579f83c7a09ac4bcd2
Remco Bloemen <remco@0x.org>
2019-12-18 20:23:08 -0800

    Static fields

--- algebra/primefield/src/const_generics.rs
+++ algebra/primefield/src/const_generics.rs
@@ -3,27 +3,53 @@ use std::marker::PhantomData;
 use std::ops::Add;
 use std::fmt;
 
+// Compile time constant modulus (constant modulus, resolved compile time)
+// Compile time constant field type (static write-once modulus, resolved compile time)
+// Fully dynamic (dynamic write-once modulus, resolved run time)
+
 pub trait Field: Sized + fmt::Debug + Add<Output = Self> {
-    const MODULUS: U256;
-    const ZERO: Self;
-    const ONE: Self;
+    fn modulus() -> U256;
+    fn zero() -> Self;
+    fn one() -> Self;
 
     fn from_u256(value: U256) -> Self;
 }
 
-pub trait PrimeFieldParameters {
-    const MODULUS: U256;
+pub struct PrimeFieldParameters {
+    modulus: U256,
 }
 
-pub struct PrimeField<F: PrimeFieldParameters> {
+//
+// Constant modulus
+//
+
+pub trait ConstantPrimeFieldParameters {
+    const PARAMETERS: PrimeFieldParameters;
+}
+
+pub struct PrimeField<F: ConstantPrimeFieldParameters> {
     value: U256,
     _field: PhantomData<F>,
 }
 
-impl<F: PrimeFieldParameters> Field for PrimeField<F> {
-    const MODULUS: U256 = F::MODULUS;
+impl<F: ConstantPrimeFieldParameters> PrimeField<F> {
+    const MODULUS: U256 = F::PARAMETERS.modulus;
     const ZERO: Self = PrimeField { value: U256::ZERO, _field: PhantomData };
     const ONE: Self = PrimeField { value: U256::ONE, _field: PhantomData };
+}
+
+impl<F: ConstantPrimeFieldParameters> Field for PrimeField<F> {
+    fn modulus() -> U256 {
+        Self::MODULUS
+    }
+
+    fn zero() -> Self {
+        Self::ZERO
+    }
+
+    fn one() -> Self {
+        Self::ONE
+    }
 
     fn from_u256(mut value: U256) -> Self {
         value %= Self::MODULUS;
@@ -31,19 +57,75 @@ impl<F: PrimeFieldParameters> Field for PrimeField<F> {
     }
 }
 
-impl<F: PrimeFieldParameters> fmt::Debug for PrimeField<F> {
+impl<F: ConstantPrimeFieldParameters> fmt::Debug for PrimeField<F> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:?}", self.value)
     }
 }
 
-impl<F: PrimeFieldParameters> Add for PrimeField<F> {
+impl<F: ConstantPrimeFieldParameters> Add for PrimeField<F> {
     type Output = PrimeField<F>;
     fn add(self, other: Self) -> Self::Output {
         // TODO: Overflow
         PrimeField::from_u256(self.value.clone() + &other.value)
     }
 }
+
+//
+// Static modulus
+//
+
+pub trait StaticPrimeFieldParameters {
+    // Ideally we'd have a 
+    // const PARAMATERS: &'a PrimeFieldParameters
+    // here, but Rust does not allow this.
+    fn parameters() -> &'static PrimeFieldParameters;
+}
+
+pub struct StaticField<F: StaticPrimeFieldParameters> {
+    value: U256,
+    _field: PhantomData<F>,
+}
+
+impl<F: StaticPrimeFieldParameters> StaticField<F> {
+    // const PARAMETERS: &'static PrimeFieldParameters = F::PARAMETERS;
+    const ZERO: Self = StaticField { value: U256::ZERO, _field: PhantomData };
+    const ONE: Self = StaticField { value: U256::ONE, _field: PhantomData };
+}
+
+impl<F: StaticPrimeFieldParameters> Field for StaticField<F> {
+    fn modulus() -> U256 {
+        F::parameters().modulus.clone()
+    }
+
+    fn zero() -> Self {
+        Self::ZERO
+    }
+
+    fn one() -> Self {
+        Self::ONE
+    }
+
+    fn from_u256(mut value: U256) -> Self {
+        value %= &F::parameters().modulus;
+        StaticField { value, _field: PhantomData }
+    }
+}
+
+impl<F: StaticPrimeFieldParameters> fmt::Debug for StaticField<F> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self.value)
+    }
+}
+
+impl<F: StaticPrimeFieldParameters> Add for StaticField<F> {
+    type Output = StaticField<F>;
+    fn add(self, other: Self) -> Self::Output {
+        // TODO: Overflow
+        StaticField::from_u256(self.value.clone() + &other.value)
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -56,20 +138,55 @@ mod tests {
         let b = F::from_u256(U256::from(234234323));
         let c = a + b;
         dbg!(&c);
-        let c = c + F::ONE;
+        let c = c + F::one();
         dbg!(&c);
     }
 
     #[test]
-    fn test_element() {
+    fn test_constant() {
+        // Modulus is compile time constant
+        const MODULUS: U256 = u256h!("000000000000000000000000000000000000000000000000000000000000000a");
+
         // Create a type F that implements a Prime Field 
         struct SmallFieldParameters();
-        impl PrimeFieldParameters for SmallFieldParameters {
+        impl ConstantPrimeFieldParameters for SmallFieldParameters {
             // 11
-            const MODULUS: U256 = u256h!("000000000000000000000000000000000000000000000000000000000000000a");
+            const PARAMETERS: PrimeFieldParameters = PrimeFieldParameters {
+                modulus: MODULUS
+            };
         }
         type SmallField = PrimeField<SmallFieldParameters>;
 
         generic_test::<SmallField>();
+    }
+
+    #[test]
+    fn test_static() {
+        // Modulus is runtime computed
+        let prime = vec![2,3,5,7,11,13,17,19];
+        for modulus in prime {
+            let modulus = U256::from(modulus);
+
+            static mut FIELD_PARAMETERS: PrimeFieldParameters = PrimeFieldParameters {
+                modulus: U256::ZERO
+            };
+            unsafe {
+                // Setting modulus is unsafe while field elements exist.
+                FIELD_PARAMETERS.modulus = modulus;
+            }
+
+            // Create a type F that implements a Prime Field
+            struct SmallFieldParameters();
+            impl StaticPrimeFieldParameters for SmallFieldParameters {
+                fn parameters() -> &'static PrimeFieldParameters {
+                    unsafe {
+                        &FIELD_PARAMETERS
+                    }
+                }
+            }
+            type SmallField = StaticField<SmallFieldParameters>;
+
+            generic_test::<SmallField>();
+        }
     }
 }
