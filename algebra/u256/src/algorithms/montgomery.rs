8161bbfc70577709c998851ec7a9c2b1c53e7aac
Remco Bloemen <remco@0x.org>
2019-12-23 22:54:56 -0800

    Add proth_mul_redc

--- algebra/u256/src/algorithms/montgomery.rs
+++ algebra/u256/src/algorithms/montgomery.rs
@@ -201,9 +201,9 @@ pub fn mul_redc<M: Parameters>(x: &U256, y: &U256) -> U256 {
 #[allow(clippy::shadow_unrelated)]
 #[inline(always)]
 pub fn mul_redc_inline<M: Parameters>(x: &U256, y: &U256) -> U256 {
-    let (lo, hi) = x.mul_full_inline(y);
-    return proth_redc_inline::<M>(&lo, &hi);
-
+    //let (lo, hi) = x.mul_full_inline(y);
+    //return proth_redc_inline::<M>(&lo, &hi);
+    return proth_mul_redc_inline::<M>(x, y);
 
     let x = x.as_limbs();
     let modulus = M::MODULUS.as_limbs();
@@ -259,6 +259,82 @@ pub fn mul_redc_inline<M: Parameters>(x: &U256, y: &U256) -> U256 {
     let (_a, carry) = mac(a0, k, modulus[0], 0);
     let (a0, carry) = mac(a1, k, modulus[1], carry);
     let (a1, carry) = mac(a2, k, modulus[2], carry);
+    let (a2, carry) = mac(a3, k, modulus[3], carry);
+    let a3 = a4 + carry;
+
+    // Final reduction
+    let mut r = U256::from_limbs([a0, a1, a2, a3]);
+    if r >= M::MODULUS {
+        r -= &M::MODULUS;
+    }
+    r
+}
+
+// We rebind variables for readability
+#[allow(clippy::shadow_unrelated)]
+#[inline(always)]
+pub fn proth_mul_redc_inline<M: Parameters>(x: &U256, y: &U256) -> U256 {
+    let modulus = M::MODULUS.as_limbs();
+    let m3 = modulus[3];
+    assert_eq!(modulus[0], 1);
+    assert_eq!(modulus[1], 0);
+    assert_eq!(modulus[2], 0);
+    assert_eq!(M::M64, u64::max_value());
+
+    let x = x.as_limbs();
+    let y = y.as_limbs();
+
+    let k = x[0].wrapping_mul(y[0]).wrapping_neg();
+    let (a0, carry) = mac(0, x[0], y[0], 0);
+    let (a1, carry) = mac(0, x[0], y[1], carry);
+    let (a2, carry) = mac(0, x[0], y[2], carry);
+    let (a3, carry) = mac(0, x[0], y[3], carry);
+    let a4 = carry;
+    let (_a, carry) = adc(a0, k, 0);
+    let (a0, carry) = adc(a1, 0, carry);
+    let (a1, carry) = adc(a2, 0, carry);
+    let (a2, carry) = mac(a3, k, m3, carry);
+    let a3 = a4 + carry;
+    let k = x[1]
+        .wrapping_mul(y[0])
+        .wrapping_add(a0)
+        .wrapping_neg();
+    let (a0, carry) = mac(a0, x[1], y[0], 0);
+    let (a1, carry) = mac(a1, x[1], y[1], carry);
+    let (a2, carry) = mac(a2, x[1], y[2], carry);
+    let (a3, carry) = mac(a3, x[1], y[3], carry);
+    let a4 = carry;
+    let (_a, carry) = adc(a0, k, 0);
+    let (a0, carry) = adc(a1, 0, carry);
+    let (a1, carry) = adc(a2, 0, carry);
+    let (a2, carry) = mac(a3, k, m3, carry);
+    let a3 = a4 + carry;
+    let k = x[2]
+        .wrapping_mul(y[0])
+        .wrapping_add(a0)
+        .wrapping_neg();
+    let (a0, carry) = mac(a0, x[2], y[0], 0);
+    let (a1, carry) = mac(a1, x[2], y[1], carry);
+    let (a2, carry) = mac(a2, x[2], y[2], carry);
+    let (a3, carry) = mac(a3, x[2], y[3], carry);
+    let a4 = carry;
+    let (_a, carry) = adc(a0, k, 0);
+    let (a0, carry) = adc(a1, 0, carry);
+    let (a1, carry) = adc(a2, 0, carry);
+    let (a2, carry) = mac(a3, k, m3, carry);
+    let a3 = a4 + carry;
+    let k = x[3]
+        .wrapping_mul(y[0])
+        .wrapping_add(a0)
+        .wrapping_neg();
+    let (a0, carry) = mac(a0, x[3], y[0], 0);
+    let (a1, carry) = mac(a1, x[3], y[1], carry);
+    let (a2, carry) = mac(a2, x[3], y[2], carry);
+    let (a3, carry) = mac(a3, x[3], y[3], carry);
+    let a4 = carry;
+    let (_a, carry) = adc(a0, k, 0);
+    let (a0, carry) = adc(a1, 0, carry);
+    let (a1, carry) = adc(a2, 0, carry);
     let (a2, carry) = mac(a3, k, modulus[3], carry);
     let a3 = a4 + carry;
 
