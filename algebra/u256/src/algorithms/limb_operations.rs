80bfe4e7500dbacf0272276e3ac1d6935b51bb66
Remco Bloemen <remco@0x.org>
2019-12-24 00:28:03 -0800

    Normalize borrow to {0, 1}

--- algebra/u256/src/algorithms/limb_operations.rs
+++ algebra/u256/src/algorithms/limb_operations.rs
@@ -10,11 +10,10 @@ pub const fn adc(a: u64, b: u64, carry: u64) -> (u64, u64) {
 /// Compute a - (b + borrow), returning the result and the new borrow.
 #[inline(always)]
 pub const fn sbb(a: u64, b: u64, borrow: u64) -> (u64, u64) {
-    // TODO: Why this shift on borrow?
-    let ret = (a as u128).wrapping_sub((b as u128) + ((borrow >> 63) as u128));
+    let ret = (a as u128).wrapping_sub((b as u128) + (borrow as u128));
     // We want truncation here
     #[allow(clippy::cast_possible_truncation)]
-    (ret as u64, (ret >> 64) as u64)
+    (ret as u64, (ret >> 127) as u64)
 }
 
 /// Compute a + (b * c) + carry, returning the result and the new carry over.
