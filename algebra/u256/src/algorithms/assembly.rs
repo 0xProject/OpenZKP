d0811dc6da7f716006e9917a804329b0a1183d36
Remco Bloemen <remco@0x.org>
2020-01-10 15:15:48 -0800

    Reduce_1 routing

--- algebra/u256/src/algorithms/assembly.rs
+++ algebra/u256/src/algorithms/assembly.rs
@@ -125,6 +125,64 @@ pub fn mul_add_1_asm(
     r4
 }
 
+/// Reduce at most once
+#[inline(always)]
+#[cfg(all(target_arch = "x86_64", target_feature = "adx"))]
+pub fn reduce_1(
+    s0: u64,
+    s1: u64,
+    s2: u64,
+    s3: u64,
+    d0: &mut u64,
+    d1: &mut u64,
+    d2: &mut u64,
+    d3: &mut u64,
+    m0: u64,
+    m1: u64,
+    m2: u64,
+    m3: u64,
+) {
+    unsafe {
+        asm!(r"
+        // Copy [s0..3] into [d0..3]
+        mov $4, $0
+        mov $5, $1
+        mov $6, $2
+        mov $7, $3
+
+        // Subtract [m0..3] from [s0..3] in place
+        // TODO: Move up for better pipelining
+        sub $8, $4
+        sbb $9, $5
+        sbb $10, $6
+        sbb $11, $7
+
+        // Conditionally copy [s0..3] into [d0..3] when no carry
+        cmovnc $4, $0
+        cmovnc $5, $1
+        cmovnc $6, $2
+        cmovnc $7, $3
+        "
+        : // Output constraints
+            "=rm"(*d0),   // $0 d0..3 are in register or memory
+            "=rm"(*d1),   // $1
+            "=rm"(*d2),   // $2
+            "=rm"(*d3),   // $3
+        : // Input constraints
+            "r"(s0),      // $4 s0..3 are in registers
+            "r"(s1),      // $5
+            "r"(s2),      // $6
+            "r"(s3),      // $7
+            "rmi"(m0)     // $8 m0..3 are in registers or memory or immediate
+            "rmi"(m1)     // $9
+            "rmi"(m2)     // $10
+            "rmi"(m3)     // $11
+        : // Clobbers
+           "cc"         // Flags
+        )
+    }
+}
+
 #[inline(always)]
 pub fn full_mul_asm2(x: &U256, y: &U256) -> (U256, U256) {
     let x = x.as_limbs();
