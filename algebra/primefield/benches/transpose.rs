c84d397dcfe36451a9e01d20374fa6c23fb0f15d
Remco Bloemen <remco@0x.org>
2020-03-02 13:14:13 -0800

    Better transpose benches

--- algebra/primefield/benches/transpose.rs
+++ algebra/primefield/benches/transpose.rs
@@ -8,83 +8,132 @@ use zkp_primefield::{
 };
 use zkp_u256::U256;
 
-const SIZES: [usize; 6] = [64, 1024, 16384, 262144, 4194304, 16777216];
+const SIZES: [usize; 4] = [16384, 262144, 4194304, 16777216];
+const N2N: [usize; 4] = [131072, 524288, 2097152, 8388608];
 
 fn bench_square(crit: &mut Criterion) {
-    log_size_bench(crit, "Transpose square size", &SIZES, move |bench, size| {
-        let log2 = size.trailing_zeros() as usize;
-        assert_eq!(log2 % 2, 0);
-        let rows = 1_usize << (log2 / 2);
-        let cols = 1_usize << (log2 / 2);
-        let src: Vec<_> = (0..rows * cols).map(FieldElement::from).collect();
-        let mut dst = src.clone();
-        bench.iter(|| transpose(&src, &mut dst, rows))
-    });
+    log_size_bench(
+        crit,
+        "Transpose n ⨉ n size",
+        &SIZES,
+        move |bench, size| {
+            let log2 = size.trailing_zeros() as usize;
+            let rows = 1_usize << (log2 / 2);
+            let cols = 1_usize << (log2 / 2);
+            assert_eq!(rows * cols, size);
+            let src: Vec<_> = (0..rows * cols).map(FieldElement::from).collect();
+            let mut dst = src.clone();
+            bench.iter(|| transpose(&src, &mut dst, cols))
+        },
+    );
 }
 
 fn bench_strip(crit: &mut Criterion) {
-    log_size_bench(crit, "Transpose strip size", &SIZES, move |bench, size| {
-        let rows = size / 8;
-        let cols = 8;
-        let src: Vec<_> = (0..rows * cols).map(FieldElement::from).collect();
-        let mut dst = src.clone();
-        bench.iter(|| transpose(&src, &mut dst, rows))
-    });
+    log_size_bench(
+        crit,
+        "Transpose n ⨉ 8 size",
+        &SIZES,
+        move |bench, size| {
+            let rows = size / 8;
+            let cols = 8;
+            assert_eq!(rows * cols, size);
+            let src: Vec<_> = (0..rows * cols).map(FieldElement::from).collect();
+            let mut dst = src.clone();
+            bench.iter(|| transpose(&src, &mut dst, cols))
+        },
+    );
 }
 
 fn bench_square_inplace(crit: &mut Criterion) {
     log_size_bench(
         crit,
-        "Transpose square in-place size",
+        "Transpose in-place n ⨉ n size",
         &SIZES,
         move |bench, size| {
             let log2 = size.trailing_zeros() as usize;
-            assert_eq!(log2 % 2, 0);
             let rows = 1_usize << (log2 / 2);
             let cols = 1_usize << (log2 / 2);
+            assert_eq!(rows * cols, size);
             let src: Vec<_> = (0..rows * cols).map(FieldElement::from).collect();
             let mut dst = src.clone();
-            bench.iter(|| transpose_inplace(&mut dst, rows))
+            bench.iter(|| transpose_inplace(&mut dst, cols))
         },
     );
 }
 
-fn bench_reference_square(crit: &mut Criterion) {
+fn bench_rectangle_inplace(crit: &mut Criterion) {
     log_size_bench(
         crit,
-        "Transpose reference square size",
-        &SIZES,
+        "Transpose in-place n ⨉ 2n size",
+        &N2N,
         move |bench, size| {
             let log2 = size.trailing_zeros() as usize;
-            assert_eq!(log2 % 2, 0);
             let rows = 1_usize << (log2 / 2);
-            let cols = 1_usize << (log2 / 2);
+            let cols = 2_usize << (log2 / 2);
+            assert_eq!(rows * cols, size);
             let src: Vec<_> = (0..rows * cols).map(FieldElement::from).collect();
             let mut dst = src.clone();
-            bench.iter(|| reference(&src, &mut dst, rows))
+            bench.iter(|| transpose_inplace(&mut dst, cols))
         },
     );
 }
 
-fn bench_reference_strip(crit: &mut Criterion) {
+fn bench_rectangle2_inplace(crit: &mut Criterion) {
     log_size_bench(
         crit,
-        "Transpose reference strip size",
-        &SIZES,
+        "Transpose in-place 2n ⨉ n size",
+        &N2N,
+        move |bench, size| {
+            let log2 = size.trailing_zeros() as usize;
+            let rows = 2_usize << (log2 / 2);
+            let cols = 1_usize << (log2 / 2);
+            assert_eq!(rows * cols, size);
+            let src: Vec<_> = (0..rows * cols).map(FieldElement::from).collect();
+            let mut dst = src.clone();
+            bench.iter(|| transpose_inplace(&mut dst, cols))
+        },
+    );
+}
+
+fn bench_strip_inplace(crit: &mut Criterion) {
+    log_size_bench(
+        crit,
+        "Transpose in-place n ⨉ 8 size",
+        &N2N,
         move |bench, size| {
             let rows = size / 8;
             let cols = 8;
+            assert_eq!(rows * cols, size);
             let src: Vec<_> = (0..rows * cols).map(FieldElement::from).collect();
             let mut dst = src.clone();
-            bench.iter(|| reference(&src, &mut dst, rows))
+            bench.iter(|| transpose_inplace(&mut dst, cols))
         },
     );
 }
+
+fn bench_strip2_inplace(crit: &mut Criterion) {
+    log_size_bench(
+        crit,
+        "Transpose in-place 8 ⨉ n size",
+        &N2N,
+        move |bench, size| {
+            let rows = 8;
+            let cols = size / 8;
+            assert_eq!(rows * cols, size);
+            let src: Vec<_> = (0..rows * cols).map(FieldElement::from).collect();
+            let mut dst = src.clone();
+            bench.iter(|| transpose_inplace(&mut dst, cols))
+        },
+    );
+}
+
 criterion_group!(
     group,
     bench_square,
     bench_strip,
     bench_square_inplace,
-    /* bench_reference_square,
-     * bench_reference_strip, */
+    bench_rectangle_inplace,
+    bench_rectangle2_inplace,
+    bench_strip_inplace,
+    bench_strip2_inplace,
 );
