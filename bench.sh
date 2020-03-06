8c562c8db6f3555097a2488a0c14069a70ad1257
Remco Bloemen <remco@0x.org>
2020-03-06 10:33:51 -0800

    Remote benchmarking

---
+++ bench.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Helper script to execute benchmarks on remote (AWS) Linux machines.
+
+MACHINE="$1"
+TARGET="x86_64-unknown-linux-musl"
+shift
+
+rm target/$TARGET/release/benchmark-*
+cargo build --release --package zkp-primefield --target $TARGET  --bench benchmark --all-features
+BENCH=(target/$TARGET/release/benchmark-*)
+scp $BENCH $MACHINE:~/benchmark
+ssh -t $MACHINE "~/benchmark" --color --bench "$@"
