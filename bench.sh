5a12e49b50bc47a6cc4acb49601117a44691d5a1
Remco Bloemen <remco@0x.org>
2020-03-06 12:07:55 -0800

    Propagate fails

--- bench.sh
+++ bench.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Helper script to execute benchmarks on remote (AWS) Linux machines.
-
+set -e
 MACHINE="$1"
 TARGET="x86_64-unknown-linux-musl"
 shift
 
-rm target/$TARGET/release/benchmark-*
+rm target/$TARGET/release/benchmark-* || true
 cargo build --release --package zkp-primefield --target $TARGET  --bench benchmark --all-features
 BENCH=(target/$TARGET/release/benchmark-*)
 scp $BENCH $MACHINE:~/benchmark
