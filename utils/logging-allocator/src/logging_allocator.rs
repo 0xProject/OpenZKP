855b2f18b7e02b2985c02fd657567a5678e4b476
Remco Bloemen <remco@0x.org>
2020-03-07 22:39:25 -0800

    Remove allocation block

--- utils/logging-allocator/src/logging_allocator.rs
+++ utils/logging-allocator/src/logging_allocator.rs
@@ -29,7 +29,7 @@ impl LoggingAllocator {
             info:              1_000_000,
             warn:              10_000_000,
             error:             100_000_000,
-            reject:            10_000_000_000,
+            reject:            1_000_000_000_000,
             allocated:         AtomicUsize::new(0),
             peak_allocated:    AtomicUsize::new(0),
             total_allocated:   AtomicUsize::new(0),
