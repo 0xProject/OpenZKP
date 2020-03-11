95dbbd3f15743de0c81ac66a8b885f942f3a0164
Remco Bloemen <remco@0x.org>
2020-03-10 21:13:28 -0700

    Make prefetch work on stable

--- algebra/primefield/src/fft/prefetch.rs
+++ algebra/primefield/src/fft/prefetch.rs
@@ -1,29 +1,18 @@
-// TODO: Move to utils crate
-
-// TODO: Use crate or builtins.
+// TODO: Move to a utils crate
 
 // TODO: Add madvise  <http://man7.org/linux/man-pages/man2/madvise.2.html>
-// TODO: MADV_HUGEPAGE
 
 // TODO: Specify cache level instead of defaulting to L1.
 
 // TODO: implement IndexPrefetch<I> for Index<I>
 
+// Use the _mm_prefetch intrinsic from stable for now.
 #[cfg(target_arch = "x86_64")]
-extern "C" {
-    /// See <http://llvm.org/docs/LangRef.html#llvm-prefetch-intrinsic>
-    #[link_name = "llvm.prefetch"]
-    fn llvm_prefetch(a: *const i8, b: i32, c: i32, d: i32) -> ();
-}
-
-const READ: i32 = 0;
-const WRITE: i32 = 1;
-const L1: i32 = 3;
-const DATA: i32 = 1;
+use std::arch::x86_64::{_mm_prefetch, _MM_HINT_T0};
 
 pub trait Prefetch {
     /// Prefetch for reading
-    fn prefetch(&self);
+    fn prefetch_read(&self);
 
     /// Prefetch for writing.
     /// (We don't need `&mut` here because we are not yet changing anything)
@@ -35,7 +24,7 @@ where
     I: ?Sized,
 {
     /// Prefetch an element from a collection by index
-    fn prefetch_index(&self, index: I);
+    fn prefetch_index_read(&self, index: I);
 
     /// Prefetch an element for writing from a collection by index
     /// (We don't need `&mut` here because we are not yet changing anything)
@@ -45,33 +34,27 @@ where
 // Blanket implementation for all types
 impl<T> Prefetch for T {
     #[inline(always)]
-    fn prefetch(&self) {
+    fn prefetch_read(&self) {
         // Prefetching does not affect the semantics of the program.
         #[allow(unsafe_code)]
         unsafe {
             #[allow(trivial_casts)] // False positive
             let ptr = self as *const T as *const i8;
-            llvm_prefetch(ptr, READ, L1, DATA);
+            _mm_prefetch(ptr, _MM_HINT_T0);
         }
     }
 
     #[inline(always)]
     fn prefetch_write(&self) {
-        #[cfg(target_arch = "x86_64")]
-        // Prefetching does not affect the semantics of the program.
-        #[allow(unsafe_code)]
-        unsafe {
-            #[allow(trivial_casts)] // False positive
-            let ptr = self as *const T as *const i8;
-            llvm_prefetch(ptr, WRITE, L1, DATA);
-        }
+        // Currently no intrinsic available, so do a read prefetch instead.
+        self.prefetch_read()
     }
 }
 
 // Blanket implementation for slices
 impl<T> PrefetchIndex<usize> for [T] {
     #[inline(always)]
-    fn prefetch_index(&self, index: usize) {
+    fn prefetch_index_read(&self, index: usize) {
         // Bounds checking is not necessary for prefetches.
         // Prefetches do not change the semantics and even if the prefetch
         // causes a page fault or any other memory exception, it is silently
@@ -80,21 +63,13 @@ impl<T> PrefetchIndex<usize> for [T] {
         unsafe {
             #[allow(trivial_casts)] // False positive
             let ptr = self.get_unchecked(index) as *const T as *const i8;
-            llvm_prefetch(ptr, READ, L1, DATA);
+            _mm_prefetch(ptr, _MM_HINT_T0);
         }
     }
 
     #[inline(always)]
     fn prefetch_index_write(&self, index: usize) {
-        // Bounds checking is not necessary for prefetches.
-        // Prefetches do not change the semantics and even if the prefetch
-        // causes a page fault or any other memory exception, it is silently
-        // ignored by the CPU.
-        #[allow(unsafe_code)]
-        unsafe {
-            #[allow(trivial_casts)] // False positive
-            let ptr = self.get_unchecked(index) as *const T as *const i8;
-            llvm_prefetch(ptr, WRITE, L1, DATA);
-        }
+        // No intrinsic for write available on stable yet, do a read instead
+        self.prefetch_index_read(index)
     }
 }
