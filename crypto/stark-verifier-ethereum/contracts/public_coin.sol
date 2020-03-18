c9ed4cba606dc1a0a39507f5f78f4fb854cef457
Paul Vienhage <paul@0x.org>
2020-03-18 17:07:25 -0400

    fixed warnings

--- crypto/stark-verifier-ethereum/contracts/public_coin.sol
+++ crypto/stark-verifier-ethereum/contracts/public_coin.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.4;
 import './hashing.sol';
 
 contract PublicCoin is Hashing {
-    
+
     struct Coin {
         bytes32 digest;
         uint64 counter;
@@ -11,7 +11,7 @@ contract PublicCoin is Hashing {
 
     // Takes bytes to be written to the channel and writes them to the coin,
     // Note that because this is a memory refrence this updates the coin.
-    function write_bytes32(bytes32 to_be_written, Coin memory coin) internal {
+    function write_bytes32(bytes32 to_be_written, Coin memory coin) internal pure {
         bytes32 hashed = double_width_hash(coin.digest, to_be_written);
         coin.counter = 0;
         coin.digest = hashed;
@@ -19,14 +19,14 @@ contract PublicCoin is Hashing {
 
     // Uses the digest and counter of the coin to create a random number
     // Note that because this is a memory refrence this updates the coin.
-    function read_bytes32(Coin memory coin) internal returns(bytes32) {
+    function read_bytes32(Coin memory coin) internal pure returns(bytes32) {
         bytes32 hashed = double_width_hash(coin.digest, bytes32(uint256(coin.counter)));
         coin.counter ++;
         return hashed;
     }
 
     // Adds a new coin with the starting bytes of data hashed to form the digest.
-    function init_coin(bytes memory starting_data) internal returns(Coin memory) {
+    function init_coin(bytes memory starting_data) internal pure returns(Coin memory) {
         bytes32 hashed = hasher(starting_data);
         Coin memory new_coin;
         new_coin.digest = hashed;
