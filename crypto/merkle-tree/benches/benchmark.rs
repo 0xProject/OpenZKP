9ee726d4d88c8372b8b9fb6b024bc581c3cd8c36
Remco Bloemen <remco@0x.org>
2020-03-23 16:57:31 -0700

    Avoid criterion macros

--- crypto/merkle-tree/benches/benchmark.rs
+++ crypto/merkle-tree/benches/benchmark.rs
@@ -1,10 +1,14 @@
 #![warn(clippy::all)]
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, Criterion};
 use zkp_criterion_utils::{log_size_bench, log_thread_bench};
 use zkp_merkle_tree::Tree;
 use zkp_u256::U256;
 
+#[cfg(not(test))]
 const SIZES: [usize; 6] = [64, 256, 1024, 4096, 16384, 65536];
+
+#[cfg(test)]
+const SIZES: [usize; 1] = [64];
 
 fn merkle_tree_size(crit: &mut Criterion) {
     log_size_bench(crit, "Merkle tree size", &SIZES, move |bench, size| {
@@ -21,10 +25,9 @@ fn merkle_tree_threads(crit: &mut Criterion) {
     });
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    merkle_tree_size(c);
-    merkle_tree_threads(c);
+fn main() {
+    let crit = &mut Criterion::default().configure_from_args();
+    merkle_tree_size(crit);
+    merkle_tree_threads(crit);
+    crit.final_summary();
 }
-
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
