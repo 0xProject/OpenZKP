ca1077da5258db032888ca3049f2110566fb6ddc
Paul Vienhage <paul@0x.org>
2020-03-24 14:02:01 -0400

    fixed comments

--- crypto/stark-verifier-ethereum/contracts/primefield.sol
+++ crypto/stark-verifier-ethereum/contracts/primefield.sol
@@ -1,35 +1,22 @@
 pragma solidity ^0.6.4;
 
 contract PrimeField {
-    uint256 constant internal K_MODULUS = 0x800000000000011000000000000000000000000000000000000000000000001;
-    uint256 constant internal K_MODULUS_MASK = 0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
-    uint256 constant internal K_MONTGOMERY_R = 0x7fffffffffffdf0ffffffffffffffffffffffffffffffffffffffffffffffe1;
-    uint256 constant internal K_MONTGOMERY_R_INV = 0x40000000000001100000000000012100000000000000000000000000000000;
+    uint256 constant internal MODULUS = 0x800000000000011000000000000000000000000000000000000000000000001;
+    uint256 constant internal MODULUS_MASK = 0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+    uint256 constant internal MONTGOMERY_R = 0x7fffffffffffdf0ffffffffffffffffffffffffffffffffffffffffffffffe1;
+    uint256 constant internal MONTGOMERY_R_INV = 0x40000000000001100000000000012100000000000000000000000000000000;
     uint256 constant internal GENERATOR = 3;
-    uint256 constant internal ONE_VAL = 1;
-    uint256 constant internal gen1024_val = 0x659d83946a03edd72406af6711825f5653d9e35dc125289a206c054ec89c4f1;
+    uint256 constant internal ONE = 1;
 
-    function from_montgomery(uint256 val) internal pure returns (uint256 res) {
-        assembly {
-            res := mulmod(val, K_MONTGOMERY_R_INV,
-                K_MODULUS)
-        }
-        return res;
+    function from_montgomery(uint256 value) internal pure returns (uint256 result) {
+        return mulmod(value, MONTGOMERY_R_INV, MODULUS);
     }
 
     function from_montgomery_bytes(bytes32 bs) internal pure returns (uint256) {
         /// Assuming bs is a 256bit bytes object, in montgomery form, it is read into a field
         /// element
-        uint256 res = uint256(bs);
-        return from_montgomery(res);
-    }
-
-    // This is an unchecked cast and should be used very carefully,
-    // and only in cases when the data is already in the right form.
-    function from_bytes_raw(bytes32 bs) internal pure returns(uint256 data) {
-        assembly {
-            data := bs
-        }
+        uint256 result = uint256(bs);
+        return from_montgomery(result);
     }
 
     // This is an unchecked cast and should be used very carefully,
@@ -40,68 +27,28 @@ contract PrimeField {
         }
     }
 
-    function to_montgomery_int(uint256 val) internal pure returns (uint256 res) {
-        assembly {
-            res := mulmod(val, K_MONTGOMERY_R,
-                K_MODULUS)
-        }
-        return res;
+    function to_montgomery_int(uint256 value) internal pure returns (uint256 result) {
+        return mulmod(value, MONTGOMERY_R, MODULUS);
     }
 
-    function fmul(uint256 a, uint256 b) internal pure returns (uint256 res) {
-        assembly {
-            res := mulmod(a, b,
-                K_MODULUS)
-        }
-        return res;
+    function fmul(uint256 a, uint256 b) internal pure returns (uint256 result) {
+        return mulmod(a, b, MODULUS);
     }
 
-    function fadd(uint256 a, uint256 b) internal pure returns (uint256 res) {
-        assembly {
-            res := addmod(a, b,
-                K_MODULUS)
-        }
-        return res;
+    function fadd(uint256 a, uint256 b) internal pure returns (uint256 result) {
+        return addmod(a, b, MODULUS);
     }
 
-    function fsub(uint256 a, uint256 b) internal pure returns (uint256 res) {
-        assembly {
-            res := addmod(a, sub(K_MODULUS, b),
-                K_MODULUS)
-        }
-        return res;
+    function fsub(uint256 a, uint256 b) internal pure returns (uint256 result) {
+        return addmod(a, MODULUS - b, MODULUS);
     }
 
-    function fdiv(uint256 a, uint256 b) internal returns (uint256) {
-        // this function has not been optimized as we don't expect to use it due to batch inverse
-        uint256 b_inv = inverse(b);
-        uint256 res = fmul(a, b_inv);
-        return res;
+    function fpow(uint256 value, uint256 exp) internal returns (uint256) {
+        return expmod(value, exp, MODULUS);
     }
 
-    function fpow(uint256 val, uint256 exp) internal returns (uint256) {
-        if (exp < 20) {
-            return fpow2(val, exp);
-        } else {
-            return expmod(val, exp, K_MODULUS);
-        }
-    }
-
-    function fpow2(uint256 val, uint256 exp) internal pure returns (uint256) {
-        uint256 cur_pow = val;
-        uint n = exp;
-        uint256 res = 1;
-        while (n > 0) {
-            if ((n % 2) != 0) {
-                res = fmul(res, cur_pow);
-            }
-            n = n / 2;
-            cur_pow = fmul(cur_pow, cur_pow);
-        }
-        return res;
-    }
-
-    function expmod(uint256 base, uint256 exponent, uint256 modulus) internal returns (uint256 res) {
+    // There's still no native call to the exp mod precompile in solidity
+    function expmod(uint256 base, uint256 exponent, uint256 modulus) internal returns (uint256 result) {
         assembly {
             let p := mload(0x40)
             mstore(p, 0x20)             // Length of Base
@@ -114,11 +61,11 @@ contract PrimeField {
             if iszero(call(not(0), 0x05, 0, p, 0xc0, p, 0x20)) {
                 revert(0, 0)
             }
-            res := mload(p)
+            result := mload(p)
         }
     }
 
-    function inverse(uint256 val) internal returns (uint256) {
-        return expmod(val, K_MODULUS - 2, K_MODULUS);
+    function inverse(uint256 value) internal returns (uint256) {
+        return expmod(value, MODULUS - 2, MODULUS);
     }
 }
--- crypto/stark-verifier-ethereum/contracts/testing_contracts/prime_field_tester.sol
+++ crypto/stark-verifier-ethereum/contracts/testing_contracts/prime_field_tester.sol
@@ -6,11 +6,11 @@ contract PrimeFieldTester is PrimeField {
 
     event log_bytes32(bytes32 data);
 
-    function fmul_external(uint256 a, uint256 b) external pure returns (uint256 res) {
+    function fmul_external(uint256 a, uint256 b) external pure returns (uint256) {
         return fmul(a, b);
     }
 
-    function fadd_external(uint256 a, uint256 b) external pure returns (uint256 res) {
+    function fadd_external(uint256 a, uint256 b) external pure returns (uint256) {
         return fadd(a, b);
     }
 
