a18cb48bcab94cab59e567e1b00be124203ae1fb
Remco Bloemen <remco@0x.org>
2020-03-28 18:02:31 -0700

    Test extrinsic equality

--- crypto/stark/src/component2/vertical.rs
+++ crypto/stark/src/component2/vertical.rs
@@ -43,25 +43,19 @@ where
     // TODO: Vectorize the claim? Encode claim in a lookup polynomial?
     fn constraints(&self, claim: &Self::Claim) -> Vec<RationalExpression> {
         use RationalExpression::*;
-        let constraints = self
-            .element
+        self.element
             // TODO: Avoid `unwrap`
-            .constraints(claim.first().unwrap());
-        if self.size > 1 {
-            constraints
-                .into_iter()
-                .map(|expression| {
-                    expression.map(&|node| {
-                        match node {
-                            X => X.pow(self.size),
-                            other => other,
-                        }
-                    })
+            .constraints(claim.first().unwrap())
+            .into_iter()
+            .map(|expression| {
+                expression.map(&|node| {
+                    match node {
+                        X => X.pow(self.size),
+                        other => other,
+                    }
                 })
-                .collect::<Vec<_>>()
-        } else {
-            constraints
-        }
+            })
+            .collect::<Vec<_>>()
     }
 
     fn trace(&self, claim: &Self::Claim, witness: &Self::Witness) -> TraceTable {
@@ -130,11 +124,47 @@ mod tests {
             let component = Vertical::new(element.clone(), 1);
             let claim_vec = vec![claim.clone(); 1];
             let witness_vec = vec![witness.clone(); 1];
-            prop_assert_eq!(component.constraints(&claim_vec), element.constraints(&claim));
+            for (result, expected) in component.constraints(&claim_vec).iter()
+                .zip(element.constraints(&claim).iter()) {
+                // We expect extrinsic equality, but not intrinsic.
+                prop_assert!(result.equals(expected));
+            }
             prop_assert_eq!(component.trace(&claim_vec, &witness_vec), element.trace(&claim, &witness));
         });
     }
 
-    // TODO: Test `Vertical::new(Vertical::new(A, n), m) == Vertical::new(A, n *
-    // m)`
+    // Test `Vertical::new(Vertical::new(A, n), m) == Vertical::new(A, n * m)`
+    #[test]
+    fn test_compose() {
+        let witness = (0_usize..4, 0_usize..4).prop_flat_map(|(log_inner_size, log_outer_size)| {
+            vec(
+                vec(any::<FieldElement>(), 1 << log_inner_size),
+                1 << log_outer_size,
+            )
+        });
+        proptest!(|(
+            log_rows in 0_usize..5,
+            cols in 0_usize..10,
+            seed: FieldElement,
+            claim: FieldElement,
+            witness in witness,
+        )| {
+            let outer_size = witness.len();
+            let inner_size = witness.first().unwrap().len();
+            // dbg!(outer_size, inner_size);
+            let element_rows = 1 << log_rows;
+            let element = Test::new(element_rows, cols, &seed);
+            let inner = Vertical::new(element.clone(), inner_size);
+            let outer = Vertical::new(inner, outer_size);
+            let component = Vertical::new(element, outer_size * inner_size);
+            let claim_vec = vec![claim.clone(); outer_size * inner_size];
+            let witness_vec = witness.iter().flatten().cloned().collect::<Vec<_>>();
+            let claim = vec![vec![claim; inner_size]; outer_size];
+            for (result, expected) in outer.constraints(&claim).iter()
+                .zip(component.constraints(&claim_vec).iter()) {
+                prop_assert!(result.equals(expected));
+            }
+            prop_assert_eq!(outer.trace(&claim, &witness), component.trace(&claim_vec, &witness_vec));
+        });
+    }
 }
