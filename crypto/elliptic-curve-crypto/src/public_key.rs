f6329d06dbe2da89c55e33851123053abebb3caa
Mason Liang <mason@0x.org>
2020-04-06 19:36:30 -0400

    Fix build

--- crypto/elliptic-curve-crypto/src/public_key.rs
+++ crypto/elliptic-curve-crypto/src/public_key.rs
@@ -38,7 +38,7 @@ impl PublicKey {
 
 impl From<&PrivateKey> for PublicKey {
     fn from(private_key: &PrivateKey) -> Self {
-        Self(Affine::from(&base_mul(&*GENERATOR_TABLE, &private_key.0)))
+        Self(Affine::from(&base_mul(&*GENERATOR_TABLE, private_key.as_scalar_field_element())))
     }
 }
 
@@ -60,10 +60,10 @@ mod tests {
         let private_key = PrivateKey::from(u256h!(
             "03c1e9550e66958296d11b60f8e8e7a7ad990d07fa65d5f7652c4a6c87d4e3cc"
         ));
-        let expected = PublicKey::new(
+        let expected = PublicKey::from(Affine::new(
             field_element!("077a3b314db07c45076d11f62b6f9e748a39790441823307743cf00d6597ea43"),
             field_element!("054d7beec5ec728223671c627557efc5c9a6508425dc6c900b7741bf60afec06"),
-        );
+        ));
         let result = PublicKey::from(&private_key);
         assert_eq!(result, expected);
     }
@@ -73,10 +73,10 @@ mod tests {
         let digest = ScalarFieldElement::from(u256h!(
             "01e542e2da71b3f5d7b4e9d329b4d30ac0b5d6f266ebef7364bf61c39aac35d0"
         ));
-        let public_key = PublicKey::new(
+        let public_key = PublicKey::from(Affine::new(
             field_element!("077a3b314db07c45076d11f62b6f9e748a39790441823307743cf00d6597ea43"),
             field_element!("054d7beec5ec728223671c627557efc5c9a6508425dc6c900b7741bf60afec06"),
-        );
+        ));
         let signature = Signature::new(
             ScalarFieldElement::from(u256h!(
                 "01ef15c18599971b7beced415a40f0c7deacfd9b0d1819e03d723d8bc943cfca"
