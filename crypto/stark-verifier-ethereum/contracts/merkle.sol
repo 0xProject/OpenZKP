e076c1a2984ef74f2ff20a6a06b70a36d56bd67e
Remco Bloemen <remco@0x.org>
2020-04-30 21:04:20 -0700

    Add more documentation

--- crypto/stark-verifier-ethereum/contracts/merkle.sol
+++ crypto/stark-verifier-ethereum/contracts/merkle.sol
@@ -29,39 +29,49 @@ contract MerkleVerifier is Trace {
             decommitment := add(decommitment, 0x20)
 
             // Set up ring buffer
+            // Every next layer will have equal or fewer values, so write_index
+            // can never overrun read_index.
             let read_index := 0
             let write_index := 0
 
             for {} 1 {} {
-                // Read the current index and store leaf hash in scratch space
+                // Read the current index
                 let index := mload(add(indices, read_index))
+                // Store leaf hash in scratch space at 0x00 or 0x20 depending on
+                // the lower bit of index (which indicates left or right node)
                 mstore(shl(5, and(index, 1)), mload(add(leaves, read_index)))
+                // Increment read pointer, wrappering around the end
                 read_index := mod(add(read_index, 0x20), length)
 
-                // Stop if we hit the root
+                // Stop if we hit the root, which has index 1
                 if eq(index, 1) {
-                    // Root hash is stored right.
+                    // Root hash is stored right
                     valid := eq(mload(0x20), root)
                     break
                 }
 
-                // Check if the next available index is the right sibbling.
-                // `index | 1` turns index it to the right sibbling (no-op if it already is)
+                // Check if the next index in the ring is the right sibbling.
+                // `index | 1` turns index into the right sibbling (no-op if it already is)
                 switch eq(or(index, 1), mload(add(indices, read_index)))
                 case 0 {
-                    // No merge, read a decommitment
+                    // No merge with right sibbling, read a decommitment
+                    // Decommitment goes in left or right, opposite of the index bit.
+                    mstore(shl(5, and(not(index), 1)), mload(decommitment))
                     // It doesn't matter if we read decommitment beyond the end,
                     // we would read in garbage and not produce a valid root.
-                    mstore(shl(5, and(not(index), 1)), mload(decommitment))
                     decommitment := add(decommitment, 0x20)
                 }
                 default {
-                    // Merging with next item in ring buffer
+                    // Merg with next item in ring buffer, which is the right sibbling.
+                    // Current must be a left. Right sibbling hash goes into 0x20.
                     mstore(0x20, mload(add(leaves, read_index)))
                     read_index := mod(add(read_index, 0x20), length)
                 }
+                // New node index is half the current index
                 mstore(add(indices, write_index), shr(1, index))
+                // New node left and right leaf are stored in 0x00..0x40
                 mstore(add(leaves, write_index), and(keccak256(0x00, 0x40), HASH_MASK))
+                // Increment and wrap the write pointer
                 write_index := mod(add(write_index, 0x20), length)
             }
         }
