144211de05cba5842d10ca776f414a9de0d88242
Remco Bloemen <remco@0x.org>
2020-08-04 19:53:25 -0700

    Randomized field arithmetic benchmarks

--- algebra/primefield/benches/field.rs
+++ algebra/primefield/benches/field.rs
@@ -1,53 +1,41 @@
 #![warn(clippy::all)]
 use criterion::{black_box, Criterion};
-use zkp_macros_decl::u256h;
+use rand::prelude::*;
 use zkp_primefield::{FieldElement, Inv, SquareRoot};
-use zkp_u256::U256;
 
 fn field_add(crit: &mut Criterion) {
-    let a = FieldElement::from_montgomery(u256h!(
-        "03f9b5d66dd1e8ef70ead1370f862cc9c29e319a176e9f5b7f10c24c4de29f0f"
-    ));
-    let b = FieldElement::from_montgomery(u256h!(
-        "0560d4ae8cd8a5974b122d8cf65967e5c83911ed0c74f02899727b3f2e916e23"
-    ));
     crit.bench_function("Field add", move |bench| {
-        bench.iter(|| black_box(&a).clone() + black_box(&b).clone())
+        let a: &FieldElement = &random();
+        let b: &FieldElement = &random();
+        bench.iter(|| black_box(a) + black_box(b))
     });
 }
 
 fn field_mul(crit: &mut Criterion) {
-    let a = FieldElement::from_montgomery(u256h!(
-        "03f9b5d66dd1e8ef70ead1370f862cc9c29e319a176e9f5b7f10c24c4de29f0f"
-    ));
-    let b = FieldElement::from_montgomery(u256h!(
-        "0560d4ae8cd8a5974b122d8cf65967e5c83911ed0c74f02899727b3f2e916e23"
-    ));
     crit.bench_function("Field mul", move |bench| {
+        let a: &FieldElement = &random();
+        let b: &FieldElement = &random();
         bench.iter(|| {
-            black_box(black_box(&a) * black_box(&b));
+            black_box(black_box(a) * black_box(b));
         })
     });
 }
 
 fn field_inv(crit: &mut Criterion) {
-    let a = FieldElement::from_montgomery(u256h!(
-        "03f9b5d66dd1e8ef70ead1370f862cc9c29e319a176e9f5b7f10c24c4de29f0f"
-    ));
     crit.bench_function("Field inv", move |bench| {
+        let a: &FieldElement = &random();
         bench.iter(|| {
-            black_box(black_box(&a).clone().inv());
+            black_box(black_box(a).inv());
         })
     });
 }
 
 fn field_sqrt(crit: &mut Criterion) {
-    let a = FieldElement::from_montgomery(u256h!(
-        "03f9b5d66dd1e8ef70ead1370f862cc9c29e319a176e9f5b7f10c24c4de29f0f"
-    ));
     crit.bench_function("Field square root", move |bench| {
+        let a: &FieldElement = &random();
+        let a = &(a * a);
         bench.iter(|| {
-            black_box(black_box(&a).square_root());
+            black_box(black_box(a).square_root());
         })
     });
 }
