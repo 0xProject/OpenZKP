2bdf18d7ac64597e0b11c14107975b08c133969b
Remco Bloemen <remco@0x.org>
2020-08-04 20:14:19 -0700

    Benchmark more field ops

--- algebra/primefield/benches/field.rs
+++ algebra/primefield/benches/field.rs
@@ -1,7 +1,7 @@
 #![warn(clippy::all)]
 use criterion::{black_box, Criterion};
 use rand::prelude::*;
-use zkp_primefield::{FieldElement, Inv, SquareRoot};
+use zkp_primefield::{FieldElement, Inv, SquareInline, SquareRoot};
 
 fn field_add(crit: &mut Criterion) {
     crit.bench_function("Field add", move |bench| {
@@ -11,12 +11,36 @@ fn field_add(crit: &mut Criterion) {
     });
 }
 
+fn field_sub(crit: &mut Criterion) {
+    crit.bench_function("Field sub", move |bench| {
+        let a: &FieldElement = &random();
+        let b: &FieldElement = &random();
+        bench.iter(|| black_box(a) - black_box(b))
+    });
+}
+
+fn field_neg(crit: &mut Criterion) {
+    crit.bench_function("Field neg", move |bench| {
+        let a: &FieldElement = &random();
+        bench.iter(|| -black_box(a))
+    });
+}
+
 fn field_mul(crit: &mut Criterion) {
     crit.bench_function("Field mul", move |bench| {
         let a: &FieldElement = &random();
         let b: &FieldElement = &random();
         bench.iter(|| {
             black_box(black_box(a) * black_box(b));
+        })
+    });
+}
+
+fn field_square(crit: &mut Criterion) {
+    crit.bench_function("Field square", move |bench| {
+        let a: &FieldElement = &random();
+        bench.iter(|| {
+            black_box(black_box(a).square());
         })
     });
 }
@@ -42,7 +66,10 @@ fn field_sqrt(crit: &mut Criterion) {
 
 pub fn group(crit: &mut Criterion) {
     field_add(crit);
+    field_sub(crit);
+    field_neg(crit);
     field_mul(crit);
+    field_square(crit);
     field_inv(crit);
     field_sqrt(crit);
 }
